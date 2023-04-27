c95d552914842ad9611d368653087e135be10360
rehohoho <ernie.birdy.ho@gmail.com>
2023-04-27 14:53:51 -0700

    Conv: change from window inputs to const params for weights and bias

--- design/aie_src/conv.cc
+++ design/aie_src/conv.cc
@@ -2,15 +2,20 @@
 #include "kernel_utils.h"
 
 
+/*
+weight+bias const: 249103 cycles
+weight const: 314790 cycles
+weight load: 93331*28=2613268 cycles
+*/
 template <int INP_W, int OUT_W, int B, int C, int M, int K>
-void conv_relu_scalar(
+void ConvReluScalar<INP_W, OUT_W, B, C, M, K>::filter(
 	input_window<float>* in,      // BHWC (1x28x28x1)
-  input_window<float>* weight,  // MKKC (6x5x5x1)
-  input_window<float>* bias,    // M    (6)
   output_window<float>* out     // BHWM (1x24x24x6)
 ) {
   PROFILE_HEADER;
   printf("Running conv_relu_scalar<%d, %d, %d, %d, %d, %d>", INP_W, OUT_W, B, C, M, K);
+
+  int weightIdx = 0;
 
   // BHWM
   for (int b = 0; b < B; b++) {
@@ -20,29 +25,26 @@ void conv_relu_scalar(
         for (int m = 0; m < M; m++) { 
 
           // KKC
-          float res = window_readincr(bias);
+          float res = bias[m];
           
           for (int p = 0; p < K; p++) {
             for (int q = 0; q < K; q++) {
               for (int c = 0; c < C; c++) {
-                float a = window_readincr(in);
-                float b = window_readincr(weight);
-                // if (m == 0) printf("%f ", a);
-                res += a * b;
+                res += window_readincr(in) * weights[weightIdx];
+                weightIdx++;
               }
             }
             window_incr(in, C*(-K + INP_W)); // go back K, go down 1
-            // if (m == 0) printf("\n");
           }
 
           if (res < 0) res = 0;
           window_writeincr(out, res);
           window_incr(in, C*(-K*INP_W)); // go up K
         }
-
-        window_incr(in, C); // next position
+        weightIdx = 0;                   // reset weight
+        window_incr(in, C);              // next position
       }
-      window_incr(in, C*K - C); // next row
+      window_incr(in, C*K - C);          // next row
     }
   }
 
--- design/aie_src/conv.h
+++ design/aie_src/conv.h
@@ -6,11 +6,32 @@
 
 
 template <int INP_W, int OUT_W, int B, int C, int M, int K>
-void conv_relu_scalar(
-	input_window<float>* in,      // BHWC (1x28x28x1)
-  input_window<float>* weight,  // MKKC (6x5x5x1)
-  input_window<float>* bias,    // M    (6)
-  output_window<float>* out     // BHWM (1x24x24x6)
-);
+class ConvReluScalar {
+
+  private:
+    alignas(32) float weights[M*K*K*C];
+    alignas(32) float bias[M];
+
+  public:
+    ConvReluScalar(
+      const float (&w)[M*K*K*C], // only accepts reference to MKKC array
+      const float (&b)[M]
+    ) {
+      for (int i = 0; i < M*K*K*C; i++)
+        weights[i] = w[i];
+      for (int i = 0; i < M; i++)
+        bias[i] = b[i];
+    };
+
+    void filter(
+      input_window<float>* in,      // BHWC (1x28x28x1)
+      output_window<float>* out     // BHWM (1x24x24x6)
+    );
+    
+    static void registerKernelClass() {
+      REGISTER_FUNCTION(ConvReluScalar::filter);
+    }
+
+};
 
 #endif // CONV_H_
