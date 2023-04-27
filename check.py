435c0b3baef075b00021e2e2ad3dc0cf72f18388
rehohoho <ernie.birdy.ho@gmail.com>
2023-04-26 17:26:17 -0700

    Python: upgrade check script to check matched files in dir

--- check.py
+++ check.py
@@ -1,19 +1,51 @@
 import argparse
+import os
 import sys
 
 import numpy as np
 
+DATA_DIR = "data/"
+OUT_DIR = "reports_dir/net_x1/sw_emu/x86simulator_output"
 
-if __name__ == "__main__":
-  parser = argparse.ArgumentParser(prog=sys.argv[0], description='Compare diff between two files.')
-  parser.add_argument('f1')
-  parser.add_argument('f2')
-  args = sys.argv
-  args.pop(0)
-  args = parser.parse_args(args)
 
-  arr1 = np.loadtxt(args.f1)
-  arr2 = np.loadtxt(args.f2)
+def load_txt(filepath: str):
+  with open(filepath, "r") as f:
+    raw = f.read().split("\n")
+  data = []
+  for line in raw:
+    if "TLAST" not in line and "T " not in line and line != "":
+      data.append([float(i) for i in line.split()])
+  return np.array(data)
+
+
+def check(filepath1: str,
+          filepath2: str):
+  arr1 = load_txt(filepath1)
+  arr2 = load_txt(filepath2)
   assert arr1.shape ==  arr2.shape
   np.testing.assert_allclose(arr1, arr2, rtol=1e-03, atol=1e-05)
   print(f"TEST: ok! (shapes match and within rtol=1e-03, atol=1e-05)")
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(prog=sys.argv[0], 
+                                   description="Compare diff between two files.")
+  parser.add_argument("-f1")
+  parser.add_argument("-f2")
+  args = parser.parse_args()
+
+  if args.f1 and args.f2 and os.path.isfile(args.f1) and os.path.isfile(args.f2):
+    print(f"Checking {args.f1} against {args.f2}")
+    check(args.f1, args.f2)
+  else:
+    if args.f1 and args.f2 and os.path.isdir(args.f1) and os.path.isdir(args.f2):
+      DATA_DIR = args.f1
+      OUT_DIR = args.f2
+    
+    print(f"Checking directories {DATA_DIR} and {OUT_DIR}")
+    filenames = set(os.listdir(DATA_DIR)).intersection(os.listdir(OUT_DIR))
+    for fn in filenames:
+      print(f"Checking {fn}")
+      check(
+        os.path.join(DATA_DIR, fn), 
+        os.path.join(OUT_DIR, fn))
