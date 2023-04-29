c915b40d0b5e0dadc034cace0b9e85716873ad63
rehohoho <ernie.birdy.ho@gmail.com>
2023-04-28 18:21:49 -0700

    Concat: add graph to test concat_scalar

---
+++ design/aie_src/graph_concat.cpp
@@ -0,0 +1,35 @@
+#include "graph_concat.h"
+#include "graph_utils.h"
+
+#define ITER_CNT 1
+
+
+// instance to be compiled and used in host within xclbin
+char concat1_input[]  = "concat_in.txt";
+char concat1_empty[]  = "empty.txt";
+char concat1_output[] = "concat_out.txt";
+ConcatScalarGraph<3, 8, 8, 8, 0, concat1_input, concat1_empty, concat1_output> concat1("1");
+
+
+#ifdef __X86SIM__
+int main(int argc, char ** argv) {
+	
+  adfCheck(concat1.init(), "init concat1");
+  adfCheck(concat1.run(1), "run concat1");
+	adfCheck(concat1.end(), "end concat1");
+
+  return 0;
+}
+#endif
+
+
+#ifdef __AIESIM__
+int main(int argc, char ** argv) {
+	
+	adfCheck(concat1.init(), "init concat1");
+  get_graph_throughput_by_port(concat1, "plout[0]", concat1.plout[0], 8+8+8+8, sizeof(float), ITER_CNT);
+	adfCheck(concat1.end(), "end concat1");
+
+  return 0;
+}
+#endif
---
+++ design/aie_src/graph_concat.h
@@ -0,0 +1,62 @@
+#ifndef __GRAPH_H__
+#define __GRAPH_H__
+
+#include <adf.h>
+#include "concat.h"
+
+
+template <int LCNT, int L0, int L1, int L2, int L3,
+  const char* INP_TXT, const char* EMPTY_TXT, const char* OUT_TXT>
+class ConcatScalarGraph : public adf::graph {
+
+  private:
+    adf::kernel k[1];
+    std::string id;
+
+  public:
+    adf::input_plio plin[4];
+    adf::output_plio plout[1];
+
+    ConcatScalarGraph(const std::string& id) { 
+      this->id = id;
+
+      k[0] = adf::kernel::create(concat_scalar<L0, L1, L2, L3>);
+      adf::source(k[0]) = "concat.cc";
+      adf::runtime<ratio>(k[0]) = 0.6;
+
+      // IO
+#ifdef EXTERNAL_IO
+#define SET_PLIN(i, LSIZE) { \
+      std::string plin_name = "plin"+std::to_string(i)+"_concat"+id+"_input"; \
+      plin[i] = adf::input_plio::create(plin_name, adf::plio_64_bits); \
+      adf::connect<adf::window<LSIZE*4>> (plin[i].out[0], k[0].in[i]);}
+
+      SET_PLIN(0, L0);
+      SET_PLIN(1, L1);
+      SET_PLIN(2, L2);
+      SET_PLIN(3, L3);
+      plout[0] = adf::output_plio::create("plout0_concat"+id+"_output", adf::plio_64_bits);
+#else
+#define SET_PLIN(i, LSIZE, TXT_PATH) { \
+      std::string plin_name = "plin"+std::to_string(i)+"_concat"+id+"_input"; \
+      if (LCNT > i) { \
+        plin[i] = adf::input_plio::create(plin_name, adf::plio_64_bits, TXT_PATH); \
+        adf::connect<adf::window<LSIZE*4>> (plin[i].out[0], k[0].in[i]); \
+      } else { \
+        plin[i] = adf::input_plio::create(plin_name, adf::plio_64_bits, EMPTY_TXT); \
+        adf::connect<adf::window<4>> (plin[i].out[0], k[0].in[i]); }}
+      
+      SET_PLIN(0, L0, INP_TXT);
+      SET_PLIN(1, L1, INP_TXT);
+      SET_PLIN(2, L2, INP_TXT);
+      SET_PLIN(3, L3, INP_TXT);
+      plout[0] = adf::output_plio::create("plout0_concat"+id+"_output", adf::plio_64_bits, OUT_TXT);
+#endif
+      
+      adf::connect<adf::window<(L0+L1+L2+L3)*4>> (k[0].out[0], plout[0].in[0]);
+    }
+
+};
+
+
+#endif // __GRAPH_H__
