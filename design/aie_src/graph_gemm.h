dbcb4db13fee429bd82fe47245c897a28b61ec5b
rehohoho <ernie.birdy.ho@gmail.com>
2023-04-28 19:04:07 -0700

    Gemm: update graph to chunk computation by params

--- design/aie_src/graph_gemm.h
+++ design/aie_src/graph_gemm.h
@@ -3,19 +3,23 @@
 
 #include <adf.h>
 #include "gemm.h"
+#include "concat.h"
 
 
-template <int M, int K, int N, 
+// chunks NxK weight params since they are too large
+template <int NCHUNK, int M, int K, int N, 
   const char* INP_TXT, const char* OUT_TXT>
 class GemmReluScalarGraph : public adf::graph {
 
   private:
-    adf::kernel k[1];
+    static const int CHUNK_COUNT = N / NCHUNK + 1;
+    static constexpr int NCUTCHUNK = N % NCHUNK;
+    std::vector<adf::kernel> gemms;
     std::string id;
 
   public:
-    adf::input_plio plin[1];
-    adf::output_plio plout[1];
+    std::vector<adf::input_plio> plins;
+    std::vector<adf::output_plio> plouts;
 
     GemmReluScalarGraph(
       const std::string& id,
@@ -24,21 +28,37 @@ class GemmReluScalarGraph : public adf::graph {
     ) { 
       this->id = id;
 
-      k[0] = adf::kernel::create_object<GemmReluScalar<M, K, N>>(weights, bias);
-      adf::source(k[0]) = "gemm.cc";
+      adf::kernel k;
+      adf::input_plio plin;
+      adf::output_plio plout;
+      std::vector<float> wChunk;
+      std::vector<float> bChunk;
+
+      for (int i = 0; i < CHUNK_COUNT; i++) {
+        int chunkSize = (i*NCHUNK + NCHUNK > N) ? NCUTCHUNK : NCHUNK;
+        wChunk = std::vector<float>(weights.begin()+i*NCHUNK*K, weights.begin()+(i*NCHUNK+chunkSize)*K); 
+        wChunk.resize(NCHUNK*K, 0);
+        bChunk = std::vector<float>(bias.begin()+i*NCHUNK, bias.begin()+i*NCHUNK+chunkSize);
+        bChunk.resize(NCHUNK, 0);
+        k = adf::kernel::create_object<GemmReluScalar<M, K, NCHUNK>>(wChunk, bChunk, i*NCHUNK);
+        adf::source(k) = "gemm.cc";
+        adf::runtime<ratio>(k) = 0.6;
 
 #ifdef EXTERNAL_IO
-      plin[0] = adf::input_plio::create("plin0_gemm"+id+"_input", adf::plio_64_bits);
-      plout[0] = adf::output_plio::create("plout0_gemm"+id+"_output", adf::plio_64_bits);
+        plin = adf::input_plio::create("plin"+std::to_string(i)+"_gemm"+id+"_input", adf::plio_64_bits);
+        plout = adf::output_plio::create("plout"+std::to_string(i)+"_gemm"+id+"_output", adf::plio_64_bits);
 #else
-      plin[0] = adf::input_plio::create("plin0_gemm"+id+"_input", adf::plio_64_bits, INP_TXT);
-      plout[0] = adf::output_plio::create("plout0_gemm"+id+"_output", adf::plio_64_bits, OUT_TXT);
-#endif
-      
-      adf::connect<adf::window<M*K*4>> (plin[0].out[0], k[0].in[0]);
-      adf::connect<adf::window<M*N*4>> (k[0].out[0], plout[0].in[0]);
+        plin = adf::input_plio::create("plin"+std::to_string(i)+"_gemm"+id+"_input", adf::plio_64_bits, INP_TXT);
+        plout = adf::output_plio::create("plout"+std::to_string(i)+"_gemm"+id+"_output", adf::plio_64_bits, OUT_TXT+std::to_string(i));
+#endif  
+        
+        adf::connect<adf::window<M*K*4>> (plin.out[0], k.in[0]);
+        adf::connect<adf::window<M*NCHUNK*4>> (k.out[0], plout.in[0]);
+        gemms.push_back(k);
+        plins.push_back(plin);
+        plouts.push_back(plout);
+      }
 
-      adf::runtime<ratio>(k[0]) = 0.6;
     }
 
 };
