7cf0b788a9c7241fc31a8202d634bcf03bb396e4
rehohoho <ernie.birdy.ho@gmail.com>
2023-04-29 22:36:22 -0700

    Conv/graph: add chunk graph using concat

--- design/aie_src/graph_conv.h
+++ design/aie_src/graph_conv.h
@@ -3,10 +3,10 @@
 
 #include <adf.h>
 #include "conv.h"
+#include "concat.h"
 
 
-template <int INP_W, int OUT_W, int B, int C, int M, int K, 
-  const char* INP_TXT, const char* OUT_TXT>
+template <int INP_W, int OUT_W, int B, int C, int M, int K>
 class ConvReluScalarGraph : public adf::graph {
 
   private:
@@ -14,13 +14,15 @@ class ConvReluScalarGraph : public adf::graph {
     std::string id;
 
   public:
-    adf::input_plio plin[1];
-    adf::output_plio plout[1];
+    adf::input_plio plins[1];
+    adf::output_plio plouts[1];
 
     ConvReluScalarGraph(
       const std::string& id,
       std::vector<float> weights,
-      std::vector<float> bias
+      std::vector<float> bias,
+      const std::string& INP_TXT,
+      const std::string& OUT_TXT = "conv_out.txt"
     ) { 
       this->id = id;
 
@@ -28,17 +30,98 @@ class ConvReluScalarGraph : public adf::graph {
       adf::source(k[0]) = "conv.cc";
 
 #ifdef EXTERNAL_IO
-      plin[0] = adf::input_plio::create("plin0_conv"+id+"_input", adf::plio_64_bits);
-      plout[0] = adf::output_plio::create("plout0_conv"+id+"_output", adf::plio_64_bits);
+      plins[0] = adf::input_plio::create("plin0_conv"+id+"_input", adf::plio_64_bits);
+      plouts[0] = adf::output_plio::create("plout0_conv"+id+"_output", adf::plio_64_bits);
 #else
-      plin[0] = adf::input_plio::create("plin0_conv"+id+"_input", adf::plio_64_bits, INP_TXT);
-      plout[0] = adf::output_plio::create("plout0_conv"+id+"_output", adf::plio_64_bits, OUT_TXT);
+      plins[0] = adf::input_plio::create("plin0_conv"+id+"_input", adf::plio_64_bits, INP_TXT);
+      plouts[0] = adf::output_plio::create("plout0_conv"+id+"_output", adf::plio_64_bits, OUT_TXT);
 #endif
       
-      adf::connect<adf::window<B*INP_W*INP_W*C*4>> (plin[0].out[0], k[0].in[0]);
-      adf::connect<adf::window<B*OUT_W*OUT_W*M*4>> (k[0].out[0], plout[0].in[0]);
+      adf::connect<adf::window<B*INP_W*INP_W*C*4>> (plins[0].out[0], k[0].in[0]);
+      adf::connect<adf::window<B*OUT_W*OUT_W*M*4>> (k[0].out[0], plouts[0].in[0]);
 
       adf::runtime<ratio>(k[0]) = 0.6;
+    }
+
+};
+
+
+#define CHUNK_COUNT   M / MCHUNK + 1
+#define MCUTCHUNK     M % MCHUNK
+#define CONCAT_NLANES 8
+
+template <int MCHUNK, int INP_W, int OUT_W, int B, int C, int M, int K>
+class ConvReluScalarChunkGraph : public adf::graph {
+
+  private:
+    adf::kernel convs[CHUNK_COUNT];
+    adf::kernel concat;
+    std::string id;
+
+  public:
+    adf::input_plio plins[CONCAT_NLANES];
+    adf::output_plio plouts[1];
+    // adf::output_plio plouts[CONCAT_NLANES];
+
+    ConvReluScalarChunkGraph(
+      const std::string& id,
+      std::vector<float> weights,
+      std::vector<float> bias,
+      const std::string& INP_TXT,
+      const std::string& EMPTY_TXT,
+      const std::string& OUT_TXT = "conv_out.txt"
+    ) { 
+      this->id = id;
+
+      concat = adf::kernel::create(
+        concat8_scalar<CHUNK_COUNT, B*OUT_W*OUT_W*MCHUNK, MCHUNK, M>);
+      adf::source(concat) = "concat.cc";
+      adf::runtime<ratio>(concat) = 0.6;
+
+      std::vector<float> wChunk;
+      std::vector<float> bChunk;
+
+      for (int i = 0; i < CHUNK_COUNT; i++) {
+        int chunkSize = (i*MCHUNK + MCHUNK > M) ? MCUTCHUNK : MCHUNK;
+        wChunk = std::vector<float>(weights.begin()+i*MCHUNK*K*K*C, weights.begin()+(i*MCHUNK+chunkSize)*K*K*C); 
+        wChunk.resize(MCHUNK*K*K*C, 0);
+        bChunk = std::vector<float>(bias.begin()+i*MCHUNK, bias.begin()+i*MCHUNK+chunkSize);
+        bChunk.resize(MCHUNK, 0);
+
+        convs[i] = adf::kernel::create_object<ConvReluScalar<INP_W, OUT_W, B, C, MCHUNK, K>>(
+          wChunk, bChunk);
+        adf::source(convs[i]) = "conv.cc";
+        adf::runtime<ratio>(convs[i]) = 0.6;
+      }
+
+#ifdef EXTERNAL_IO
+#define SET_PLIN(i, TXT_PATH) { \
+      std::string plio_name = "plin"+std::to_string(i)+"_conv"+id+"_input"; \
+      plins[i] = adf::input_plio::create(plio_name, adf::plio_64_bits); }
+
+      plouts[0] = adf::output_plio::create("plout0_conv"+id+"_output", adf::plio_64_bits);
+#else
+#define SET_PLIN(i, TXT_PATH) { \
+      std::string plio_name = "plin"+std::to_string(i)+"_conv"+id+"_input"; \
+      plins[i] = adf::input_plio::create(plio_name, adf::plio_64_bits, TXT_PATH); }
+      
+      plouts[0] = adf::output_plio::create("plout0_conv"+id+"_output", adf::plio_64_bits, OUT_TXT);
+#endif
+
+      for (int i = 0; i < CONCAT_NLANES; i++) {
+        if (i < CHUNK_COUNT) {
+          SET_PLIN(i, INP_TXT);
+          adf::connect<adf::window<B*INP_W*INP_W*C*4>> (plins[i].out[0], convs[i].in[0]);
+          adf::connect<adf::window<B*OUT_W*OUT_W*MCHUNK*4>> (convs[i].out[0], concat.in[i]);
+          // plouts[i] = adf::output_plio::create("plout"+std::to_string(i)+"_conv"+id+"_output", adf::plio_64_bits, OUT_TXT+std::to_string(i));
+          // adf::connect<adf::window<B*OUT_W*OUT_W*MCHUNK*4>> (convs[i].out[0], plouts[i].in[0]);
+        } else {
+          SET_PLIN(i, EMPTY_TXT);
+          adf::connect<adf::window<4>> (plins[i].out[0], concat.in[i]);
+        }
+      }
+      
+      adf::connect<adf::window<B*OUT_W*OUT_W*M*4>> (concat.out[0], plouts[0].in[0]);
     }
 
 };
