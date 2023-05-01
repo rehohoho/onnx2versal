196c63935f3c845de1467ca006ec28c2db7283e3
rehohoho <ernie.birdy.ho@gmail.com>
2023-04-30 22:07:52 -0700

    Conv: add vector kernel for 5x5

--- design/aie_src/conv.h
+++ design/aie_src/conv.h
@@ -59,4 +59,32 @@ class ConvReluScalarBCHW {
 };
 
 
+template <int INP_W, int OUT_W, int B, int C, int M>
+class Conv5x5ReluBCHW {
+
+  private:
+    static const int K = 5;
+    alignas(32) float (&weights)[M*K*K*C];
+    alignas(32) float (&bias)[M];
+
+  public:
+    Conv5x5ReluBCHW(
+      float (&w)[M*K*K*C], // only accepts reference to MKKC array
+      float (&b)[M]
+    ): weights(w), bias(b) {}; 
+
+    void filter(
+      input_window<float>* in,      // BHWC (1x28x28x1)
+      output_window<float>* out     // BHWM (1x24x24x6)
+    );
+    
+    static void registerKernelClass() {
+      REGISTER_FUNCTION(Conv5x5ReluBCHW::filter);
+      REGISTER_PARAMETER(weights);
+      REGISTER_PARAMETER(bias);
+    }
+
+};
+
+
 #endif // CONV_H_
