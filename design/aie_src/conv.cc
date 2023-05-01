8020bafd18afa6e5bb53c2698e54aa02a5219304
rehohoho <ernie.birdy.ho@gmail.com>
2023-04-30 19:12:00 -0700

    Conv: add scalar kernel for BCHW

--- design/aie_src/conv.cc
+++ design/aie_src/conv.cc
@@ -50,3 +50,52 @@ void ConvReluScalarBHWC<INP_W, OUT_W, B, C, M, K>::filter(
 
   PROFILE_FOOTER;
 }
+
+
+template <int INP_W, int OUT_W, int B, int C, int M, int K>
+void ConvReluScalarBCHW<INP_W, OUT_W, B, C, M, K>::filter(
+	input_window<float>* in,      // BCHW (1x1x28x28)
+  output_window<float>* out     // BMHW (1x6x24x24)
+) {
+  PROFILE_HEADER;
+  printf("Running conv_relu_scalar<%d, %d, %d, %d, %d, %d>", INP_W, OUT_W, B, C, M, K);
+
+  int weightIdx = 0;
+
+  // BHWM
+  for (int b = 0; b < B; b++) {
+    for (int m = 0; m < M; m++) { 
+      
+      for (int h = 0; h < OUT_W; h++) {
+        for (int w = 0; w < OUT_W; w++) {
+        
+          float res = bias[m];
+          weightIdx = m*C*K*K;
+          
+          for (int c = 0; c < C; c++) {
+            for (int p = 0; p < K; p++) {
+              for (int q = 0; q < K; q++) {
+                float a = window_readincr(in);
+                res += a * weights[weightIdx];
+                weightIdx++;
+                // if (w == 0) printf("%f ", a);
+              }
+              window_incr(in, -K+INP_W); // go left K, down 1
+              // if (w == 0) printf("\n");
+            }
+            window_incr(in, -K*INP_W + INP_W*INP_W); // go up K, channel 1
+          }
+          // if (w == 0) printf("\n");
+
+          if (res < 0) res = 0;
+          window_writeincr(out, res);
+          window_incr(in, -C*INP_W*INP_W + 1); // go channel -C, right 1
+        }
+        window_incr(in, INP_W-OUT_W); // go left OUT_W, go down 1
+      }
+      window_incr(in, -OUT_W*INP_W); // go up OUT_W
+    }
+  }
+
+  PROFILE_FOOTER;
+}
--- design/aie_src/conv.h
+++ design/aie_src/conv.h
@@ -31,4 +31,32 @@ class ConvReluScalarBHWC {
 
 };
 
+
+template <int INP_W, int OUT_W, int B, int C, int M, int K>
+class ConvReluScalarBCHW {
+
+  private:
+    alignas(32) float (&weights)[M*K*K*C];
+    alignas(32) float (&bias)[M];
+
+  public:
+    ConvReluScalarBCHW(
+      float (&w)[M*K*K*C], // only accepts reference to MKKC array
+      float (&b)[M]
+    ): weights(w), bias(b) {}; 
+
+    void filter(
+      input_window<float>* in,      // BHWC (1x28x28x1)
+      output_window<float>* out     // BHWM (1x24x24x6)
+    );
+    
+    static void registerKernelClass() {
+      REGISTER_FUNCTION(ConvReluScalarBCHW::filter);
+      REGISTER_PARAMETER(weights);
+      REGISTER_PARAMETER(bias);
+    }
+
+};
+
+
 #endif // CONV_H_
