f026e75d380917354aa0990667da95d9aa034600
rehohoho <ernie.birdy.ho@gmail.com>
2023-05-01 18:05:19 -0700

    Utils: add print vector utils

--- design/aie_src/kernel_utils.h
+++ design/aie_src/kernel_utils.h
@@ -6,3 +6,17 @@
 #define PROFILE_FOOTER \
   cycle_num[1] = tile.cycles(); \
   printf("start = %d,end = %d,total = %d\n", cycle_num[0], cycle_num[1], cycle_num[1] - cycle_num[0]);
+
+template<typename VECTYPE>
+void print_fvec(VECTYPE* vec, int N) {
+  for (int i = 0; i < N; i++) 
+    printf("%f ", vec[i]);
+  printf("\n");
+}
+
+template<typename VECTYPE>
+void print_vec(VECTYPE* vec, int N) {
+  for (int i = 0; i < N; i++) 
+    printf("%d ", vec[i]);
+  printf("\n");
+}
