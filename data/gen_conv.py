a3094b90a2a6bccfbc439746f7564cc95781acf2
rehohoho <ernie.birdy.ho@gmail.com>
2023-05-04 22:00:57 -0700

    Conv/graph: add stronger tests

--- data/gen_conv.py
+++ data/gen_conv.py
@@ -1,6 +1,16 @@
 import numpy as np
+np.random.seed(0)
 
-inp = np.arange(6*12*12).reshape(-1, 2)
-weights = np.arange(16*6*5*5).reshape(-1, 2)
-bias = np.ones((16)).reshape(-1, 2)
+C = 6
+W = 12
+M = 8
+K = 5
+inp = np.arange(C*W*W).reshape(-1, 2)
+weights = np.arange(M*C*K*K).flatten().tolist()
+bias = np.ones((M)).flatten().tolist()
 np.savetxt("conv_fpin.txt", inp)
+
+inp_rand = np.random.random(C*W*W).reshape(-1, 2)
+weights_rand = np.random.random(M*C*K*K).flatten().tolist()
+bias_rand = np.random.random((M)).flatten().tolist()
+np.savetxt("conv_fpin_rand.txt", inp_rand)
