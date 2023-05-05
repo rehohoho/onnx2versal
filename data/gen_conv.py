5a980be3b3cfa71baf9b579b4dd91c4dc92bae47
rehohoho <ernie.birdy.ho@gmail.com>
2023-05-04 18:12:42 -0700

    Conv: update test gen for better test

--- data/gen_conv.py
+++ data/gen_conv.py
@@ -1,9 +1,8 @@
 import numpy as np
 
-inp = np.arange(28*28).reshape(-1, 2)
-weights = np.arange(5*5)
-weights = np.tile(weights, 6).reshape(-1, 2)
-bias = np.ones((6)).reshape(-1, 2)
+inp = np.arange(6*12*12).reshape(-1, 2)
+weights = np.arange(16*6*5*5).reshape(-1, 2)
+bias = np.ones((16)).reshape(-1, 2)
 np.savetxt("conv_in.txt", inp)
 np.savetxt("conv_weights.txt", weights)
 np.savetxt("conv_bias.txt", bias)
