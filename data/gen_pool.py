3d1b3430fc50d5c2860177fda03127defd6f0f36
rehohoho <ernie.birdy.ho@gmail.com>
2023-05-05 13:23:05 -0700

    Test: parameterize gen_pool, gen_transpose

--- data/gen_pool.py
+++ data/gen_pool.py
@@ -1,4 +1,7 @@
 import numpy as np
 np.random.seed(0)
-np.savetxt("pool_in.txt", np.arange(6*24*24).reshape(-1, 2))
-np.savetxt("pool_in_rand.txt", np.random.random(6*24*24).reshape(-1, 2))
+
+C = 6
+W = 24
+np.savetxt("pool_in.txt", np.arange(C*W*W).reshape(-1, 2))
+np.savetxt("pool_in_rand.txt", np.random.random(C*W*W).reshape(-1, 2))
--- data/gen_transpose.py
+++ data/gen_transpose.py
@@ -1,4 +1,7 @@
 import numpy as np
 np.random.seed(0)
-np.savetxt("transpose_in.txt", np.arange(4*4*16).reshape(-1, 2))
-np.savetxt("transpose_in_rand.txt", np.random.random(4*4*16).reshape(-1, 2))
+
+C = 16
+W = 4
+np.savetxt("transpose_in.txt", np.arange(C*W*W).reshape(-1, 2))
+np.savetxt("transpose_in_rand.txt", np.random.random(C*W*W).reshape(-1, 2))
