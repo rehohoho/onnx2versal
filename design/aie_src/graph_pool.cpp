f337c02150ff408ab6030e6f2a4e55a720fcf49a
rehohoho <ernie.birdy.ho@gmail.com>
2023-05-04 20:40:55 -0700

    Pool/graph: separate test graph, use template, move txt inp to args

--- design/aie_src/graph_pool.cpp
+++ design/aie_src/graph_pool.cpp
@@ -4,14 +4,38 @@
 #define ITER_CNT 1
 
 
+template <template<int, int, int, int> class POOL,
+  int INP_W, int OUT_W, int B, int C>
+class MaxpoolGraphTest : public adf::graph {
+
+  private:
+    MaxpoolGraph<POOL, INP_W, OUT_W, B, C> g;
+
+  public:
+    adf::input_plio plin[1];
+    adf::output_plio plout[1];
+
+    MaxpoolGraphTest(
+      const std::string& id,
+      const char* INP_TXT, 
+      const char* OUT_TXT
+    ) { 
+      g.construct();
+      plin[0] = adf::input_plio::create("plin0_maxpool"+id+"_input", adf::plio_64_bits, TXT_ARG(INP_TXT));
+      plout[0] = adf::output_plio::create("plout0_maxpool"+id+"_output", adf::plio_64_bits, TXT_ARG(OUT_TXT));
+      adf::connect<adf::window<B*INP_W*INP_W*C*4>> (plin[0].out[0], g.pin[0]);
+      adf::connect<adf::window<B*OUT_W*OUT_W*C*4>> (g.pout[0], plout[0].in[0]);
+    }
+};
+
+
 // instance to be compiled and used in host within xclbin
 char pool1_input[]  = "lenet_mnist__2___pool1_MaxPool___relu1_Relu_output_0__1x24x24x6.txt";
 char pool1_output[] = "lenet_mnist__2___pool1_MaxPool___pool1_MaxPool_output_0__1x12x12x6.txt";
-MaxpoolScalar<24, 12, 1, 6, pool1_input, pool1_output> pool1("1");
-
 char pool2_input[]  = "lenet_mnist__5___pool2_MaxPool___relu2_Relu_output_0__1x8x8x16.txt";
 char pool2_output[] = "lenet_mnist__5___pool2_MaxPool___pool2_MaxPool_output_0__1x4x4x16.txt";
-MaxpoolScalar<8, 4, 1, 16, pool2_input, pool2_output> pool2("2");
+MaxpoolGraphTest<MaxpoolScalarBHWC, 24, 12, 1, 6> pool1("1", pool1_input, pool1_output);
+MaxpoolGraphTest<MaxpoolScalarBHWC, 8, 4, 1, 16> pool2("2", pool2_input, pool2_output);
 
 
 #ifdef __X86SIM__
--- design/aie_src/graph_pool.h
+++ design/aie_src/graph_pool.h
@@ -1,42 +1,32 @@
-#ifndef __GRAPH_H__
-#define __GRAPH_H__
+#ifndef __POOL_GRAPH_H__
+#define __POOL_GRAPH_H__
 
 #include <adf.h>
 #include "pool.h"
 
 
-template <int INP_W, int OUT_W, int B, int C, const char* INP_TXT, const char* OUT_TXT>
-class MaxpoolScalar : public adf::graph {
+template <template<int, int, int, int> class POOL,
+  int INP_W, int OUT_W, int B, int C>
+class MaxpoolGraph : public adf::graph {
 
   private:
     adf::kernel k[1];
     std::string id;
 
   public:
-    adf::input_plio plin[1];
-    adf::output_plio plout[1];
+    adf::port<input> pin[1];
+    adf::port<output> pout[1];
 
-    MaxpoolScalar(const std::string& id) { 
-      this->id = id;
-
-      k[0] = adf::kernel::create(maxpool_scalar<INP_W, OUT_W, B, C>);
+    void construct() { 
+      k[0] = adf::kernel::create_object<POOL<INP_W, OUT_W, B, C>>();
       adf::source(k[0]) = "pool.cc";
-
-#ifdef EXTERNAL_IO
-      plin[0] = adf::input_plio::create("plin0_maxpool"+id+"_input", adf::plio_64_bits);
-      plout[0] = adf::output_plio::create("plout0_maxpool"+id+"_output", adf::plio_64_bits);
-#else
-      plin[0] = adf::input_plio::create("plin0_maxpool"+id+"_input", adf::plio_64_bits, INP_TXT);
-      plout[0] = adf::output_plio::create("plout0_maxpool"+id+"_output", adf::plio_64_bits, OUT_TXT);
-#endif
-      
-      adf::connect<adf::window<B*INP_W*INP_W*C*4>> (plin[0].out[0], k[0].in[0]);
-      adf::connect<adf::window<B*OUT_W*OUT_W*C*4>> (k[0].out[0], plout[0].in[0]);
-
       adf::runtime<ratio>(k[0]) = 0.6;
+      
+      adf::connect<adf::window<B*INP_W*INP_W*C*4>> (pin[0], k[0].in[0]);
+      adf::connect<adf::window<B*OUT_W*OUT_W*C*4>> (k[0].out[0], pout[0]);
     }
 
 };
 
 
-#endif // __GRAPH_H__
+#endif // __POOL_GRAPH_H__
