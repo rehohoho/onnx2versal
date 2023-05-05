c8a161c0b6dee93f1dd2f229b4974e7cb271ebfd
rehohoho <ernie.birdy.ho@gmail.com>
2023-05-04 20:08:39 -0700

    Concat/graph: separate test graph

--- design/aie_src/graph_concat.cpp
+++ design/aie_src/graph_concat.cpp
@@ -4,8 +4,53 @@
 #define ITER_CNT 1
 
 
+template <int LCNT, int WINDOW_SIZE, int CHUNK_SIZE, int BLOCK_SIZE>
+class ConcatGraphTest : public adf::graph {
+
+  public:
+    adf::input_plio plin[NLANES];
+    adf::output_plio plout[1];
+    ConcatGraph<LCNT, WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE> g;
+
+    ConcatGraphTest(
+      const std::string& id,
+      const std::string& INP0_TXT,
+      const std::string& INP1_TXT,
+      const std::string& INP2_TXT,
+      const std::string& INP3_TXT,
+      const std::string& INP4_TXT,
+      const std::string& INP5_TXT,
+      const std::string& INP6_TXT,
+      const std::string& INP7_TXT,
+      const std::string& OUT_TXT = "concat_out.txt"
+    ) {
+      g.construct();
+      plin[0] = adf::input_plio::create("plin0_concat"+id+"_input", adf::plio_64_bits, TXT_ARG(INP0_TXT));
+      plin[1] = adf::input_plio::create("plin1_concat"+id+"_input", adf::plio_64_bits, TXT_ARG(INP1_TXT));
+      plin[2] = adf::input_plio::create("plin2_concat"+id+"_input", adf::plio_64_bits, TXT_ARG(INP2_TXT));
+      plin[3] = adf::input_plio::create("plin3_concat"+id+"_input", adf::plio_64_bits, TXT_ARG(INP3_TXT));
+      plin[4] = adf::input_plio::create("plin4_concat"+id+"_input", adf::plio_64_bits, TXT_ARG(INP4_TXT));
+      plin[5] = adf::input_plio::create("plin5_concat"+id+"_input", adf::plio_64_bits, TXT_ARG(INP5_TXT));
+      plin[6] = adf::input_plio::create("plin6_concat"+id+"_input", adf::plio_64_bits, TXT_ARG(INP6_TXT));
+      plin[7] = adf::input_plio::create("plin7_concat"+id+"_input", adf::plio_64_bits, TXT_ARG(INP7_TXT));
+      plout[0] = adf::output_plio::create("plout0_concat"+id+"_output", adf::plio_64_bits, TXT_ARG(OUT_TXT));
+
+      for (int i = 0; i < NLANES; i++) {  
+        if (LCNT > i) {
+          adf::connect<adf::window<WINDOW_SIZE*4>> (plin[i].out[0], g.pin[i]);
+        } else {
+          adf::connect<adf::window<4>> (plin[i].out[0], g.pin[i]);
+        }
+      }
+      // BLOCK_SIZE <= WINDOW_SIZE
+      adf::connect<adf::window<WINDOW_SIZE/CHUNK_SIZE*BLOCK_SIZE*4>> (g.pout[0], plout[0].in[0]);
+    }
+
+};
+
+
 // instance to be compiled and used in host within xclbin
-ConcatScalarGraph<5, 8, 8, 4*8+4> concat1("1",
+ConcatGraphTest<5, 8, 8, 4*8+4> concat1("1",
   "concat_in.txt", // INP0_TXT
   "concat_in.txt", // INP1_TXT
   "concat_in.txt", // INP2_TXT
@@ -17,7 +62,7 @@ ConcatScalarGraph<5, 8, 8, 4*8+4> concat1("1",
   "concat1_out.txt"// OUT_TXT
 );
 
-ConcatScalarGraph<5, 8, 4, 4*4+2> concat2("2",
+ConcatGraphTest<5, 8, 4, 4*4+2> concat2("2",
   "concat_in.txt", // INP0_TXT
   "concat_in.txt", // INP1_TXT
   "concat_in.txt", // INP2_TXT
@@ -32,7 +77,6 @@ ConcatScalarGraph<5, 8, 4, 4*4+2> concat2("2",
 
 #ifdef __X86SIM__
 int main(int argc, char ** argv) {
-	
   adfCheck(concat1.init(), "init concat1");
   adfCheck(concat1.run(1), "run concat1");
 	adfCheck(concat1.end(), "end concat1");
@@ -40,7 +84,6 @@ int main(int argc, char ** argv) {
   adfCheck(concat2.init(), "init concat2");
   adfCheck(concat2.run(1), "run concat2");
 	adfCheck(concat2.end(), "end concat2");
-
   return 0;
 }
 #endif
@@ -48,7 +91,6 @@ int main(int argc, char ** argv) {
 
 #ifdef __AIESIM__
 int main(int argc, char ** argv) {
-	
 	adfCheck(concat1.init(), "init concat1");
   get_graph_throughput_by_port(concat1, "plout[0]", concat1.plout[0], 36, sizeof(float), ITER_CNT);
 	adfCheck(concat1.end(), "end concat1");
@@ -56,7 +98,6 @@ int main(int argc, char ** argv) {
   adfCheck(concat2.init(), "init concat2");
   get_graph_throughput_by_port(concat2, "plout[0]", concat2.plout[0], 36, sizeof(float), ITER_CNT);
 	adfCheck(concat2.end(), "end concat2");
-
   return 0;
 }
 #endif
--- design/aie_src/graph_concat.h
+++ design/aie_src/graph_concat.h
@@ -1,75 +1,38 @@
-#ifndef __GRAPH_H__
-#define __GRAPH_H__
+#ifndef __CONCAT_GRAPH_H__
+#define __CONCAT_GRAPH_H__
 
 #include <adf.h>
 #include "concat.h"
+#define NLANES 8
 
 
 template <int LCNT, int WINDOW_SIZE, int CHUNK_SIZE, int BLOCK_SIZE>
-class ConcatScalarGraph : public adf::graph {
+class ConcatGraph : public adf::graph {
 
   private:
     adf::kernel k[1];
-    std::string id;
-    static const int NLANES = 8;
 
   public:
-    adf::input_plio plin[NLANES];
-    adf::output_plio plout[1];
+    adf::port<input> pin[NLANES];
+    adf::port<output> pout[1];
 
-    ConcatScalarGraph(
-      const std::string& id,
-      const std::string& INP0_TXT,
-      const std::string& INP1_TXT,
-      const std::string& INP2_TXT,
-      const std::string& INP3_TXT,
-      const std::string& INP4_TXT,
-      const std::string& INP5_TXT,
-      const std::string& INP6_TXT,
-      const std::string& INP7_TXT,
-      const std::string& OUT_TXT = "concat_out.txt"
-    ) { 
-      this->id = id;
-
+    void construct() { 
       k[0] = adf::kernel::create(concat8_scalar<LCNT, WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE>);
       adf::source(k[0]) = "concat.cc";
       adf::runtime<ratio>(k[0]) = 0.6;
 
-#ifdef EXTERNAL_IO
-#define SET_PLIN(i, TXT_PATH) { \
-      std::string plin_name = "plin"+std::to_string(i)+"_concat"+id+"_input"; \
-      plin[i] = adf::input_plio::create(plin_name, adf::plio_64_bits);}
-      
-      plout[0] = adf::output_plio::create("plout0_concat"+id+"_output", adf::plio_64_bits);
-#else
-#define SET_PLIN(i, TXT_PATH) { \
-      std::string plin_name = "plin"+std::to_string(i)+"_concat"+id+"_input"; \
-      plin[i] = adf::input_plio::create(plin_name, adf::plio_64_bits, TXT_PATH);}
-      
-      plout[0] = adf::output_plio::create("plout0_concat"+id+"_output", adf::plio_64_bits, OUT_TXT);
-#endif  
-      
-      SET_PLIN(0, INP0_TXT);
-      SET_PLIN(1, INP1_TXT);
-      SET_PLIN(2, INP2_TXT);
-      SET_PLIN(3, INP3_TXT);
-      SET_PLIN(4, INP4_TXT);
-      SET_PLIN(5, INP5_TXT);
-      SET_PLIN(6, INP6_TXT);
-      SET_PLIN(7, INP7_TXT);
-
       for (int i = 0; i < NLANES; i++) {  
         if (LCNT > i) {
-          adf::connect<adf::window<WINDOW_SIZE*4>> (plin[i].out[0], k[0].in[i]);
+          adf::connect<adf::window<WINDOW_SIZE*4>> (pin[i], k[0].in[i]);
         } else {
-          adf::connect<adf::window<4>> (plin[i].out[0], k[0].in[i]);
+          adf::connect<adf::window<4>> (pin[i], k[0].in[i]);
         }
       }
       // BLOCK_SIZE <= WINDOW_SIZE
-      adf::connect<adf::window<WINDOW_SIZE/CHUNK_SIZE*BLOCK_SIZE*4>> (k[0].out[0], plout[0].in[0]);
+      adf::connect<adf::window<WINDOW_SIZE/CHUNK_SIZE*BLOCK_SIZE*4>> (k[0].out[0], pout[0]);
     }
 
 };
 
 
-#endif // __GRAPH_H__
+#endif // __CONCAT_GRAPH_H__
