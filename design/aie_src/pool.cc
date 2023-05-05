bc4df44f6161987969a5cf3a962ce244de15b1af
rehohoho <ernie.birdy.ho@gmail.com>
2023-05-05 13:21:35 -0700

    Pool: add scalar BCHW kernel

--- design/aie_src/pool.cc
+++ design/aie_src/pool.cc
@@ -4,10 +4,13 @@
 #include "kernel_utils.h"
 
 
+/*
+Assumes INP_W divisible by K
+*/
 template <int INP_W, int OUT_W, int B, int C>
 void MaxpoolScalarBHWC<INP_W, OUT_W, B, C>::filter(
-	input_window<float>* in,      // BHWC (1x28x28x1)
-  output_window<float>* out     // BPQC (1x24x24x6)
+  input_window<float>* in,      // BHWC (1x24x24x6)
+  output_window<float>* out     // BPQC (1x12x12x6)
 ) {
   PROFILE_HEADER;
   printf("Running MaxpoolScalarBHWC::filter<%d, %d, %d, %d>\n", INP_W, OUT_W, B, C);
@@ -39,6 +42,45 @@ void MaxpoolScalarBHWC<INP_W, OUT_W, B, C>::filter(
       window_incr(in, C*(K-1)*INP_W); // go down K-1
     }
   }
+
+  PROFILE_FOOTER;
+}
+
+
+/*
+Assumes INP_W divisible by K
+*/
+template <int INP_W, int OUT_W, int B, int C>
+void MaxpoolScalarBCHW<INP_W, OUT_W, B, C>::filter(
+  input_window<float>* in,      // BCHW (1x6x24x24)
+  output_window<float>* out     // BCPQ (1x6x12x12)
+) {
+  PROFILE_HEADER;
+  printf("Running MaxpoolScalarBCHW::filter<%d, %d, %d, %d>\n", INP_W, OUT_W, B, C);
+
+  const int K = INP_W / OUT_W;
+
+  for (int b = 0; b < B; b++) {
+    for (int c = 0; c < C; c++) {
+      for (int h = 0; h < OUT_W; h++) {
+        for (int w = 0; w < OUT_W; w++) {
+
+          float c = -INFINITY;
+          for (int p = 0; p < K; p++) {
+            for (int q = 0; q < K; q++) {
+              float a = window_readincr(in);
+              c = (a > c) ? a : c;
+            }
+            window_incr(in, -K+INP_W); // left K, down 1
+          }
+          window_incr(in, -K*INP_W + K); // up K, right K
+          window_writeincr(out, c);
+
+        } // W
+        window_incr(in, -INP_W + K*INP_W); // left INP_W, down K
+      } // H
+    } // C
+  } // B
 
   PROFILE_FOOTER;
 }
--- design/aie_src/pool.h
+++ design/aie_src/pool.h
@@ -9,12 +9,26 @@ template <int INP_W, int OUT_W, int B, int C>
 class MaxpoolScalarBHWC {
   public:
     void filter(
-      input_window<float>* in,      // BHWC (1x28x28x1)
-      output_window<float>* out     // BPQC (1x24x24x6)
+      input_window<float>* in,      // BHWC (1x24x24x6)
+      output_window<float>* out     // BPQC (1x12x12x6)
     );
 
     static void registerKernelClass() {
       REGISTER_FUNCTION(MaxpoolScalarBHWC::filter);
+    }
+};
+
+
+template <int INP_W, int OUT_W, int B, int C>
+class MaxpoolScalarBCHW {
+  public:
+    void filter(
+      input_window<float>* in,      // BCHW (1x6x24x24)
+      output_window<float>* out     // BCPQ (1x6x12x12)
+    );
+
+    static void registerKernelClass() {
+      REGISTER_FUNCTION(MaxpoolScalarBCHW::filter);
     }
 };
 
