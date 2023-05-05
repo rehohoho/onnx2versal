f04e2c25e67a6f4eace19cd23b0512382a5e7b5b
rehohoho <ernie.birdy.ho@gmail.com>
2023-05-04 21:02:46 -0700

    Transpose/graph: separate test graph, use template, move txt inp to args

--- design/aie_src/graph_transpose.cpp
+++ design/aie_src/graph_transpose.cpp
@@ -4,10 +4,37 @@
 #define ITER_CNT 1
 
 
+template <template<int, int, int, int> class TRANSPOSE, 
+  int B, int H, int W, int C>
+class TransposeGraphTest : public adf::graph {
+
+  private:
+    TransposeGraph<TRANSPOSE, B, H, W, C> g;
+
+  public:
+    adf::input_plio plin[1];
+    adf::output_plio plout[1];
+
+    TransposeGraphTest(
+      const std::string& id,
+      const std::string& INP_TXT,
+      const std::string& OUT_TXT = "transpose_out.txt"
+    ) { 
+      g.construct();
+      plin[0] = adf::input_plio::create("plin0_transpose"+id+"_input", adf::plio_64_bits, TXT_ARG(INP_TXT));
+      plout[0] = adf::output_plio::create("plout0_transpose"+id+"_output", adf::plio_64_bits, TXT_ARG(OUT_TXT));
+      adf::connect<adf::window<B*H*W*C*4>> (plin[0].out[0], g.pin[0]);
+      adf::connect<adf::window<B*C*H*W*4>> (g.pout[0], plout[0].in[0]);
+    }
+
+};
+
+
 // instance to be compiled and used in host within xclbin
 char transpose1_input[]  = "lenet_mnist__13___Reshape___pool2_MaxPool_output_0__1x4x4x16.txt";
 char transpose1_output[] = "lenet_mnist__13___Reshape___Reshape_output_0__1x256.txt";
-TransposeScalar<1, 4, 4, 16, transpose1_input, transpose1_output> transpose1("1");
+TransposeGraphTest<TransposeScalarBHWC2BCHW, 1, 4, 4, 16> transpose1(
+  "1", transpose1_input, transpose1_output);
 
 
 #ifdef __X86SIM__
--- design/aie_src/graph_transpose.h
+++ design/aie_src/graph_transpose.h
@@ -1,42 +1,31 @@
-#ifndef __GRAPH_H__
-#define __GRAPH_H__
+#ifndef __TRANSPOSE_GRAPH_H__
+#define __TRANSPOSE_GRAPH_H__
 
 #include <adf.h>
 #include "transpose.h"
 
 
-template <int B, int H, int W, int C, const char* INP_TXT, const char* OUT_TXT>
-class TransposeScalar : public adf::graph {
+template <template<int, int, int, int> class TRANSPOSE, 
+  int B, int H, int W, int C>
+class TransposeGraph : public adf::graph {
 
   private:
     adf::kernel k[1];
-    std::string id;
 
   public:
-    adf::input_plio plin[1];
-    adf::output_plio plout[1];
+    adf::port<input> pin[1];
+    adf::port<output> pout[1];
 
-    TransposeScalar(const std::string& id) { 
-      this->id = id;
-
-      k[0] = adf::kernel::create(bhwc2bchw_scalar<B, H, W, C>);
+    void construct() { 
+      k[0] = adf::kernel::create_object<TRANSPOSE<B, H, W, C>>();
       adf::source(k[0]) = "transpose.cc";
-
-#ifdef EXTERNAL_IO
-      plin[0] = adf::input_plio::create("plin0_transpose"+id+"_input", adf::plio_64_bits);
-      plout[0] = adf::output_plio::create("plout0_transpose"+id+"_output", adf::plio_64_bits);
-#else
-      plin[0] = adf::input_plio::create("plin0_transpose"+id+"_input", adf::plio_64_bits, INP_TXT);
-      plout[0] = adf::output_plio::create("plout0_transpose"+id+"_output", adf::plio_64_bits, OUT_TXT);
-#endif
-      
-      adf::connect<adf::window<B*H*W*C*4>> (plin[0].out[0], k[0].in[0]);
-      adf::connect<adf::window<B*C*H*W*4>> (k[0].out[0], plout[0].in[0]);
-
       adf::runtime<ratio>(k[0]) = 0.6;
+      
+      adf::connect<adf::window<B*H*W*C*4>> (pin[0], k[0].in[0]);
+      adf::connect<adf::window<B*C*H*W*4>> (k[0].out[0], pout[0]);
     }
 
 };
 
 
-#endif // __GRAPH_H__
+#endif // __TRANSPOSE_GRAPH_H__
