2bb06d31b18c77aff50abae985fd9e173d5cc06f
rehohoho <ernie.birdy.ho@gmail.com>
2023-05-08 23:16:06 -0700

    Dma: add mm2s, s2mm for floats

---
+++ design/pl_src/float_mm2s.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <hls_stream.h>
+
+typedef float dinp_t;
+typedef float dout_t;
+
+extern "C" {
+
+
+void mm2s(dinp_t* mem, hls::stream<dout_t>& s, int size) {
+#pragma HLS INTERFACE m_axi port=mem offset=slave bundle=gmem
+#pragma HLS INTERFACE axis port=s
+#pragma HLS INTERFACE s_axilite port=mem bundle=control
+#pragma HLS INTERFACE s_axilite port=size bundle=control
+#pragma HLS interface s_axilite port=return bundle=control
+
+	for(int i = 0; i < size; i++) {
+#pragma HLS PIPELINE II=1
+		dout_t x = mem[i];
+		std::cout << "mm2s: " << x << std::endl;
+		s.write(x);
+	}
+}
+
+
+}
---
+++ design/pl_src/float_s2mm.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <hls_stream.h>
+
+typedef float dinp_t;
+typedef float dout_t;
+
+extern "C" {
+
+
+void s2mm(dinp_t* mem, hls::stream<dout_t>& s, int size) {
+#pragma HLS INTERFACE m_axi port=mem offset=slave bundle=gmem
+#pragma HLS INTERFACE axis port=s
+#pragma HLS INTERFACE s_axilite port=mem bundle=control
+#pragma HLS INTERFACE s_axilite port=size bundle=control
+#pragma HLS interface s_axilite port=return bundle=control
+
+	for(int i = 0; i < size; i++) {
+#pragma HLS PIPELINE II=1
+		dout_t x = s.read();
+		std::cout << "s2mm: " << x << std::endl;
+		mem[i] = x;
+	}
+}
+
+
+}
