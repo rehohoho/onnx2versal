b8f1a49a994364a85892eb86aafc5820d32e074a
rehohoho <ernie.birdy.ho@gmail.com>
2023-05-10 14:39:21 -0700

    Gemm: separate MKNK and MKKN scalar kernels

--- design/aie_src/gemm.cc
+++ design/aie_src/gemm.cc
@@ -12,12 +12,12 @@ weights[N*K] (120x256)
 bias[N]      (120)
 */
 template <int M, int K, int NCHUNK>
-void GemmReluScalar<M, K, NCHUNK>::filter(
+void GemmReluScalarMKNK<M, K, NCHUNK>::filter(
 	input_window<float>* in,      // MxK  (1x256)
   output_window<float>* out     // MxN  (1x120)
 ) {
   PROFILE_HEADER(printf(
-    "Running gemm_relu_scalar<%d, %d, %d>\n", M, K, NCHUNK));
+    "Running GemmReluScalarMKNK<%d, %d, %d>\n", M, K, NCHUNK));
 
   int weightIdx = 0;
 
@@ -31,6 +31,38 @@ void GemmReluScalar<M, K, NCHUNK>::filter(
         res += a * b;
       }    
       
+      if (res < 0) res = 0;
+      window_writeincr(out, res);
+      window_incr(in, -K); // repeat same in row for next j
+    }
+    window_incr(in, K); // next in row for next NCHUNK
+  }
+
+  PROFILE_FOOTER;
+}
+
+
+template <int M, int K, int NCHUNK>
+void GemmReluScalarMKKN<M, K, NCHUNK>::filter(
+	input_window<float>* in,      // MxK  (1x256)   inputs
+                                // KxN  (256x120) weights
+  output_window<float>* out     // MxN  (1x120)   outputs
+) {
+  PROFILE_HEADER(printf(
+    "Running GemmReluScalarMKKN<%d, %d, %d>\n", M, K, NCHUNK));
+
+  int weightIdx = 0;
+
+  for (int i = 0; i < M; i++) {
+    for (int j = 0; j < NCHUNK; j++) {
+      float res = bias[j];
+      weightIdx = j;
+      for (int k = 0; k < K; k++) {
+        float a = window_readincr(in);
+        float b = weights[weightIdx];
+        weightIdx += NCHUNK;
+        res += a * b;
+      }    
       if (res < 0) res = 0;
       window_writeincr(out, res);
       window_incr(in, -K); // repeat same in row for next j
--- design/aie_src/gemm.h
+++ design/aie_src/gemm.h
@@ -6,14 +6,14 @@
 
 // xA^T + b as per torch,nn.Linear
 template <int M, int K, int NCHUNK>
-class GemmReluScalar {
+class GemmReluScalarMKNK {
   
   private:
     alignas(32) float (&weights)[NCHUNK*K]; // NCHUNKxK (120x256)
     alignas(32) float (&bias)[NCHUNK];      // NCHUNK   (120/?)
   
   public:
-    GemmReluScalar (
+    GemmReluScalarMKNK (
       float (&w)[NCHUNK*K],
       float (&b)[NCHUNK]
     ): weights(w), bias(b) {};
@@ -24,10 +24,37 @@ class GemmReluScalar {
     );
     
     static void registerKernelClass() {
-      REGISTER_FUNCTION(GemmReluScalar::filter);
+      REGISTER_FUNCTION(GemmReluScalarMKNK::filter);
       REGISTER_PARAMETER(weights);
       REGISTER_PARAMETER(bias);
     };
 };
+
+
+template <int M, int K, int NCHUNK>
+class GemmReluScalarMKKN {
+  
+  private:
+    alignas(32) float (&weights)[NCHUNK*K]; // KxNCHUNK (256x120)
+    alignas(32) float (&bias)[NCHUNK];      // NCHUNK   (120/?)
+  
+  public:
+    GemmReluScalarMKKN (
+      float (&w)[NCHUNK*K],
+      float (&b)[NCHUNK]
+    ): weights(w), bias(b) {};
+
+    void filter(
+      input_window<float>* in,      // MxK       (1x256)
+      output_window<float>* out     // MxNCHUNK  (1x120/?)
+    );
+    
+    static void registerKernelClass() {
+      REGISTER_FUNCTION(GemmReluScalarMKKN::filter);
+      REGISTER_PARAMETER(weights);
+      REGISTER_PARAMETER(bias);
+    };
+};
+
 
 #endif // GEMM_H_
