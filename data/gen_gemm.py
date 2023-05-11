6948ca3c09cc22df7fd160a6ed3dd5d734cf597b
rehohoho <ernie.birdy.ho@gmail.com>
2023-05-10 20:03:18 -0700

    Gemm/graph: separate MKNK, MKKN data and tests

--- data/gen_gemm.py
+++ data/gen_gemm.py
@@ -4,12 +4,23 @@ np.random.seed(0)
 M = 1
 K = 84
 N = 10
+PAD = (4 - N%4) % 4
+
 inp = np.arange(M*K).reshape(-1, 2)
-weights = np.arange(K*N).flatten().tolist()
+
+weights_mknk = np.arange(K*N).reshape(K, N)
+weights_mkkn = np.pad(weights_mknk, ((0,0),(0,PAD)), "constant", constant_values=0)
+weights_mknk = weights_mknk.flatten().tolist()
+weights_mkkn = weights_mkkn.flatten().tolist()
+
 bias = np.ones(N).flatten().tolist()
 np.savetxt("gemm_fpin.txt", inp)
 
 inp_rand = np.random.random(M*K).reshape(-1, 2)
-weights_rand = np.random.random(K*N).flatten().tolist()
+weights_mknk_rand = np.random.random(K*N).reshape(K, N)
+weights_mkkn_rand = np.pad(weights_mknk_rand, ((0,0),(0,PAD)), "constant", constant_values=0)
+weights_mknk_rand = weights_mknk_rand.flatten().tolist()
+weights_mkkn_rand = weights_mkkn_rand.flatten().tolist()
+
 bias_rand = np.random.random(N).flatten().tolist()
 np.savetxt("gemm_fpin_rand.txt", inp_rand)
