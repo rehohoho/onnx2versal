2cf48032ad71576f77fa406255ca89c265918bc9
rehohoho <ernie.birdy.ho@gmail.com>
2023-05-10 21:12:19 -0700

    Gemm: use 2accs, 2fpmac

--- design/aie_src/gemm.cc
+++ design/aie_src/gemm.cc
@@ -95,9 +95,16 @@ a0a1a7 b00 b01 b07 b08 b09 b0f
        b70 b71 b77 b77 b79 b7f
 
 a0 * b00 b01 ... b07
-393 cycles (1x84 * 84x10)
+
+1x84 * 84x10:
+  single acc/fpmac: 393 cycles, no fpmac interleaving
+  upd_w > load v16 from pointer, allow interleaving: 406 -> 698
+  interleaving loads: 406 -> 365
+
+Typically K > N for downsampling, M=1 if each net does an instance
+If chunking by N, for N%16=0, K<=128, for N%8=0, K<=256
 */
-template <int M, int K, int NCHUNK> // K%4=0, N%4=0
+template <int M, int K, int NCHUNK> // K%4=0, N%16=0
 void GemmReluMKKN<M, K, NCHUNK>::filter(
 	input_window<float>* in,      // MxK  (1x256)   inputs
                                 // KxN  (256x120) weights
@@ -111,37 +118,62 @@ void GemmReluMKKN<M, K, NCHUNK>::filter(
   float *b_ptr = (float *) bias;
   v8float zeros = null_v8float();
   v8float matA = undef_v8float();
-  v8float matB = undef_v8float();
+  v16float matB = null_v16float();
 
   for (int i = 0; i < M; i++) {
-    for (int j = 0; j < NCHUNK; j+=8) { // fpmac accsize 8
+    for (int j = 0; j < NCHUNK; j+=16) { // fpmac accsize 8, do 2
 
       v8float acc1 = *(v8float *) (b_ptr + j);
+      v8float acc2 = *(v8float *) (b_ptr + j + 8);
 
       for (int k = 0; k < K; k+=4) {
-        matA = *(v8float *) a_ptr;
-        matB = *(v8float *) w_ptr; w_ptr += NCHUNK_RND;
-        acc1 = fpmac(acc1, matA, 0, 0x00000000, matB, 0, 0x76543210);
-        matB = *(v8float *) w_ptr; w_ptr += NCHUNK_RND;
-        acc1 = fpmac(acc1, matA, 1, 0x00000000, matB, 0, 0x76543210);
-        matB = *(v8float *) w_ptr; w_ptr += NCHUNK_RND;
-        acc1 = fpmac(acc1, matA, 2, 0x00000000, matB, 0, 0x76543210);
-        matB = *(v8float *) w_ptr; w_ptr += NCHUNK_RND;
-        acc1 = fpmac(acc1, matA, 3, 0x00000000, matB, 0, 0x76543210);
-        a_ptr += 4;
+        matA = *(v8float *) a_ptr; a_ptr += 4;
+        matB = upd_w(matB, 0, *(v8float*) w_ptr);
+        acc1 = fpmac(acc1, matB, 0, 0x76543210, matA, 0, 0x00000000);
+        matB = upd_w(matB, 1, *(v8float*) (w_ptr + 8)); w_ptr += NCHUNK_RND;
+        // print_fvec<float>((float *) &matB, 16);
+        acc2 = fpmac(acc2, matB, 8, 0x76543210, matA, 0, 0x00000000);
+        matB = upd_w(matB, 0, *(v8float*) w_ptr);
+        acc1 = fpmac(acc1, matB, 0, 0x76543210, matA, 1, 0x00000000);
+        matB = upd_w(matB, 1, *(v8float*) (w_ptr + 8)); w_ptr += NCHUNK_RND;
+        // print_fvec<float>((float *) &matB, 16);
+        acc2 = fpmac(acc2, matB, 8, 0x76543210, matA, 1, 0x00000000);
+        matB = upd_w(matB, 0, *(v8float*) w_ptr);
+        acc1 = fpmac(acc1, matB, 0, 0x76543210, matA, 2, 0x00000000);
+        matB = upd_w(matB, 1, *(v8float*) (w_ptr + 8)); w_ptr += NCHUNK_RND;
+        // print_fvec<float>((float *) &matB, 16);
+        acc2 = fpmac(acc2, matB, 8, 0x76543210, matA, 2, 0x00000000);
+        matB = upd_w(matB, 0, *(v8float*) w_ptr);
+        acc1 = fpmac(acc1, matB, 0, 0x76543210, matA, 3, 0x00000000);
+        matB = upd_w(matB, 1, *(v8float*) (w_ptr + 8)); w_ptr += NCHUNK_RND;
+        // print_fvec<float>((float *) &matB, 16);
+        acc2 = fpmac(acc2, matB, 8, 0x76543210, matA, 3, 0x00000000);
+        // printf("\n");
       }
       acc1 = fpmax(acc1, zeros, 0, 0x76543210);
+      acc2 = fpmax(acc2, zeros, 0, 0x76543210);
+      // printf("\n");
+      // print_fvec<float>((float *) &acc1, 8);
+      // print_fvec<float>((float *) &acc2, 8);
+      // printf("\n");
 
       if (NCHUNK - j < 8) {
         float *acc_ptr = (float *) &acc1;
         for (int i = 0; i < NCHUNK - j; i++) {
           window_writeincr(out, acc_ptr[i]);
         }
+      } else if (NCHUNK - j < 16) {
+        window_writeincr(out, acc1);
+        float *acc_ptr = (float *) &acc2;
+        for (int i = 0; i < NCHUNK - j - 8; i++) {
+          window_writeincr(out, acc_ptr[i]);
+        }
       } else {
         window_writeincr(out, acc1);
+        window_writeincr(out, acc2);
       }
 
-      w_ptr += -K*NCHUNK_RND + 8;
+      w_ptr += -K*NCHUNK_RND + 16;
       a_ptr -= K;
     }
     w_ptr -= NCHUNK_RND;
