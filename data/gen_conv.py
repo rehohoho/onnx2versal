ff52fdd12b4341f968ad5efb59fcef6b2a220d45
rehohoho <ernie.birdy.ho@gmail.com>
2023-05-12 00:04:57 -0700

    Conv/data: gen write outputs as well

--- data/gen_conv.py
+++ data/gen_conv.py
@@ -1,16 +1,57 @@
 import numpy as np
+import torch
+
 np.random.seed(0)
 
 C = 2
 W = 28
 M = 2
 K = 5
-inp = np.arange(C*W*W).reshape(-1, 2)
-weights = np.arange(M*C*K*K).flatten().tolist()
-bias = np.ones((M)).flatten().tolist()
-np.savetxt("conv_fpin.txt", inp)
 
-inp_rand = np.random.random(C*W*W).reshape(-1, 2)
-weights_rand = np.random.random(M*C*K*K).flatten().tolist()
-bias_rand = np.random.random((M)).flatten().tolist()
-np.savetxt("conv_fpin_rand.txt", inp_rand)
+# arange
+inp = np.arange(C*W*W)
+fpweights = np.arange(M*C*K*K)
+fpbias = np.ones((M))
+np.savetxt("conv_fpin.txt", inp.reshape(-1, 2))
+np.savetxt("conv_fpweights.txt", fpweights.reshape(-1, 2))
+np.savetxt("conv_fpbias.txt", fpbias.reshape(-1, 2))
+print("fpweights\n", fpweights.flatten().tolist(), "\n\n\n")
+print("fpbias\n", fpbias.flatten().tolist(), "\n\n\n")
+
+# random
+inp_rand = np.random.random(C*W*W)
+fpweights_rand = np.random.random(M*C*K*K)
+fpbias_rand = np.random.random((M))
+np.savetxt("conv_fpin_rand.txt", inp_rand.reshape(-1, 2))
+np.savetxt("conv_fpweights_rand.txt", fpweights_rand.reshape(-1, 2))
+np.savetxt("conv_fpbias_rand.txt", fpbias_rand.reshape(-1, 2))
+print("fpweights_rand\n", fpweights_rand.flatten().tolist(), "\n\n\n")
+print("fpbias_rand\n", fpbias_rand.flatten().tolist(), "\n\n\n")
+
+# result for bchw
+res_bchw = torch.nn.functional.conv2d(
+  torch.Tensor(inp.reshape(1,C,W,W)), 
+  torch.Tensor(fpweights.reshape(M,C,K,K)), 
+  torch.Tensor(fpbias.reshape(M))).numpy()
+res_bchw_rand = torch.nn.functional.conv2d(
+  torch.Tensor(inp_rand.reshape(1,C,W,W)), 
+  torch.Tensor(fpweights_rand.reshape(M,C,K,K)), 
+  torch.Tensor(fpbias_rand.reshape(M))).numpy()
+np.savetxt("conv_fpout_ConvReluScalarBCHW.txt", res_bchw.reshape(-1, 2))
+np.savetxt("conv_fpout_ConvReluScalarBCHW_rand.txt", res_bchw_rand.reshape(-1, 2))
+np.savetxt("conv_fpout_Conv5x5ReluBCHW.txt", res_bchw.reshape(-1, 2))
+np.savetxt("conv_fpout_Conv5x5ReluBCHW_rand.txt", res_bchw_rand.reshape(-1, 2))
+
+# result for bhwc
+res_bhwc = torch.nn.functional.conv2d(
+  torch.Tensor(inp.reshape(1,W,W,C).transpose(0,3,1,2)), 
+  torch.Tensor(fpweights.reshape(M,K,K,C).transpose(0,3,1,2)), 
+  torch.Tensor(fpbias.reshape(M))).numpy().transpose(0,2,3,1)
+res_bhwc_rand = torch.nn.functional.conv2d(
+  torch.Tensor(inp_rand.reshape(1,W,W,C).transpose(0,3,1,2)), 
+  torch.Tensor(fpweights_rand.reshape(M,K,K,C).transpose(0,3,1,2)), 
+  torch.Tensor(fpbias_rand.reshape(M))).numpy().transpose(0,2,3,1)
+np.savetxt("conv_fpout_ConvReluScalarBHWC.txt", res_bhwc.reshape(-1, 2))
+np.savetxt("conv_fpout_ConvReluScalarBHWC_rand.txt", res_bhwc_rand.reshape(-1, 2))
+np.savetxt("conv_fpout_ConvReluScalarGmemParamBHWC.txt", res_bhwc.reshape(-1, 2))
+np.savetxt("conv_fpout_ConvReluScalarGmemParamBHWC_rand.txt", res_bhwc_rand.reshape(-1, 2))
