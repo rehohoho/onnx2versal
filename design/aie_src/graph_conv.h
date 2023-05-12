de578e667fb8778fa726501f95de736af9f359ab
rehohoho <ernie.birdy.ho@gmail.com>
2023-05-12 00:06:02 -0700

    Conv.graph: add graph for gmem param

--- design/aie_src/graph_conv.h
+++ design/aie_src/graph_conv.h
@@ -32,6 +32,32 @@ class ConvReluGraph : public adf::graph {
 };
 
 
+template <template<int, int, int, int, int, int> class CONV, 
+  int INP_W, int OUT_W, int B, int C, int M, int K>
+class ConvReluGmemParamGraph : public adf::graph {
+
+  private:
+    adf::kernel k[1];
+
+  public:
+    adf::port<input> pin[3];
+    adf::port<output> pout[1];
+
+    ConvReluGmemParamGraph() { 
+      k[0] = adf::kernel::create_object<CONV<INP_W, OUT_W, B, C, M, K>>();
+      adf::source(k[0]) = "conv.cc";
+
+      adf::connect<adf::window<B*INP_W*INP_W*C*4>> (pin[0], k[0].in[0]);
+      adf::connect<adf::window<M*K*K*C*4>>         (pin[1], k[0].in[1]);
+      adf::connect<adf::window<M*4>>               (pin[2], k[0].in[2]);
+      adf::connect<adf::window<B*OUT_W*OUT_W*M*4>> (k[0].out[0], pout[0]);
+
+      adf::runtime<ratio>(k[0]) = 0.6;
+    }
+
+};
+
+
 template <template<int, int, int, int, int, int> class CONV, int IS_BCHW,
   int MCHUNK, int INP_W, int OUT_W, int B, int C, int M, int K>
 class ConvReluChunkGraph : public adf::graph {
