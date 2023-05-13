830027b20f465a58aaa0d8c4cb668df1de34249b
rehohoho <ernie.birdy.ho@gmail.com>
2023-05-12 23:08:42 -0700

    Concat: add ConcatVector

--- design/aie_src/concat.cc
+++ design/aie_src/concat.cc
@@ -41,7 +41,7 @@ void ConcatScalar<LCNT, WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE>::filter8(
   output_window<float>* out
 ) {
   PROFILE_HEADER(printf(
-    "Running Concat8Scalar<%d, %d, %d>::filter8\n", WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE));
+    "Running ConcatScalar<%d, %d, %d>::filter8\n", WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE));
 
   for (int i = 0; i < WINDOW_SIZE; i+=CHUNK_SIZE) {
     int blockIdx = 0;
@@ -70,7 +70,7 @@ void ConcatScalar<LCNT, WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE>::filter7(
   output_window<float>* out
 ) {
   PROFILE_HEADER(printf(
-    "Running Concat8Scalar<%d, %d, %d>::filter7\n", WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE));
+    "Running ConcatScalar<%d, %d, %d>::filter7\n", WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE));
 
   for (int i = 0; i < WINDOW_SIZE; i+=CHUNK_SIZE) {
     int blockIdx = 0;
@@ -97,7 +97,7 @@ void ConcatScalar<LCNT, WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE>::filter6(
   output_window<float>* out
 ) {
   PROFILE_HEADER(printf(
-    "Running Concat8Scalar<%d, %d, %d>::filter6\n", WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE));
+    "Running ConcatScalar<%d, %d, %d>::filter6\n", WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE));
 
   for (int i = 0; i < WINDOW_SIZE; i+=CHUNK_SIZE) {
     int blockIdx = 0;
@@ -207,6 +207,205 @@ void ConcatScalar<LCNT, WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE>::filter1(
   for (int i = 0; i < WINDOW_SIZE; i+=CHUNK_SIZE) {
     int blockIdx = 0;
     CAT(in0);
+  }
+
+  PROFILE_FOOTER;
+}
+
+
+// assumes CHUNK_SIZE%8=0, BLOCK_SIZE%4=0 (vector writes)
+#define CAT_VEC(INP_WIN) \
+  if (blockIdx + CHUNK_SIZE <= BLOCK_SIZE) { \
+    for (int i = 0; i < CHUNK_SIZE; i+=8) \
+      window_writeincr(out, window_readincr_v8(INP_WIN)); \
+  } else if (blockIdx < BLOCK_SIZE) { \
+    for (int i = 0; i < BLOCK_SIZE - blockIdx - 7; i+=8) \
+      window_writeincr(out, window_readincr_v8(INP_WIN)); \
+    for (int i = 0; i < (BLOCK_SIZE - blockIdx) % 8; i++) \
+      window_writeincr(out, window_readincr(INP_WIN)); \
+    window_incr(INP_WIN, blockIdx + CHUNK_SIZE - BLOCK_SIZE); \
+  } \
+  blockIdx += CHUNK_SIZE;
+
+template <int LCNT, int WINDOW_SIZE, int CHUNK_SIZE, int BLOCK_SIZE>
+void ConcatVector<LCNT, WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE>::filter8(
+	input_window<float>* in0,
+	input_window<float>* in1,
+	input_window<float>* in2,
+	input_window<float>* in3,
+	input_window<float>* in4,
+	input_window<float>* in5,
+	input_window<float>* in6,
+	input_window<float>* in7,
+  output_window<float>* out
+) {
+  PROFILE_HEADER(printf(
+    "Running ConcatVector<%d, %d, %d>::filter8\n", WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE));
+
+  for (int i = 0; i < WINDOW_SIZE; i+=CHUNK_SIZE) {
+    int blockIdx = 0;
+    CAT_VEC(in0);
+    CAT_VEC(in1);
+    CAT_VEC(in2);
+    CAT_VEC(in3);
+    CAT_VEC(in4);
+    CAT_VEC(in5);
+    CAT_VEC(in6);
+    CAT_VEC(in7);
+  }
+
+  PROFILE_FOOTER;
+}
+
+template <int LCNT, int WINDOW_SIZE, int CHUNK_SIZE, int BLOCK_SIZE>
+void ConcatVector<LCNT, WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE>::filter7(
+	input_window<float>* in0,
+	input_window<float>* in1,
+	input_window<float>* in2,
+	input_window<float>* in3,
+	input_window<float>* in4,
+	input_window<float>* in5,
+	input_window<float>* in6,
+  output_window<float>* out
+) {
+  PROFILE_HEADER(printf(
+    "Running ConcatVector<%d, %d, %d>::filter7\n", WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE));
+
+  for (int i = 0; i < WINDOW_SIZE; i+=CHUNK_SIZE) {
+    int blockIdx = 0;
+    CAT_VEC(in0);
+    CAT_VEC(in1);
+    CAT_VEC(in2);
+    CAT_VEC(in3);
+    CAT_VEC(in4);
+    CAT_VEC(in5);
+    CAT_VEC(in6);
+  }
+
+  PROFILE_FOOTER;
+}
+
+template <int LCNT, int WINDOW_SIZE, int CHUNK_SIZE, int BLOCK_SIZE>
+void ConcatVector<LCNT, WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE>::filter6(
+	input_window<float>* in0,
+	input_window<float>* in1,
+	input_window<float>* in2,
+	input_window<float>* in3,
+	input_window<float>* in4,
+	input_window<float>* in5,
+  output_window<float>* out
+) {
+  PROFILE_HEADER(printf(
+    "Running ConcatVector<%d, %d, %d>::filter6\n", WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE));
+
+  for (int i = 0; i < WINDOW_SIZE; i+=CHUNK_SIZE) {
+    int blockIdx = 0;
+    CAT_VEC(in0);
+    CAT_VEC(in1);
+    CAT_VEC(in2);
+    CAT_VEC(in3);
+    CAT_VEC(in4);
+    CAT_VEC(in5);
+  }
+
+  PROFILE_FOOTER;
+}
+
+template <int LCNT, int WINDOW_SIZE, int CHUNK_SIZE, int BLOCK_SIZE>
+void ConcatVector<LCNT, WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE>::filter5(
+	input_window<float>* in0,
+	input_window<float>* in1,
+	input_window<float>* in2,
+	input_window<float>* in3,
+	input_window<float>* in4,
+  output_window<float>* out
+) {
+  PROFILE_HEADER(printf(
+    "Running ConcatVector<%d, %d, %d>::filter5\n", WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE));
+
+  for (int i = 0; i < WINDOW_SIZE; i+=CHUNK_SIZE) {
+    int blockIdx = 0;
+    CAT_VEC(in0);
+    CAT_VEC(in1);
+    CAT_VEC(in2);
+    CAT_VEC(in3);
+    CAT_VEC(in4);
+  }
+
+  PROFILE_FOOTER;
+}
+
+template <int LCNT, int WINDOW_SIZE, int CHUNK_SIZE, int BLOCK_SIZE>
+void ConcatVector<LCNT, WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE>::filter4(
+	input_window<float>* in0,
+	input_window<float>* in1,
+	input_window<float>* in2,
+	input_window<float>* in3,
+  output_window<float>* out
+) {
+  PROFILE_HEADER(printf(
+    "Running ConcatVector<%d, %d, %d>::filter4\n", WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE));
+
+  for (int i = 0; i < WINDOW_SIZE; i+=CHUNK_SIZE) {
+    int blockIdx = 0;
+    CAT_VEC(in0);
+    CAT_VEC(in1);
+    CAT_VEC(in2);
+    CAT_VEC(in3);
+  }
+
+  PROFILE_FOOTER;
+}
+
+template <int LCNT, int WINDOW_SIZE, int CHUNK_SIZE, int BLOCK_SIZE>
+void ConcatVector<LCNT, WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE>::filter3(
+	input_window<float>* in0,
+	input_window<float>* in1,
+	input_window<float>* in2,
+  output_window<float>* out
+) {
+  PROFILE_HEADER(printf(
+    "Running ConcatVector<%d, %d, %d>::filter3\n", WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE));
+
+  for (int i = 0; i < WINDOW_SIZE; i+=CHUNK_SIZE) {
+    int blockIdx = 0;
+    CAT_VEC(in0);
+    CAT_VEC(in1);
+    CAT_VEC(in2);
+  }
+
+  PROFILE_FOOTER;
+}
+
+template <int LCNT, int WINDOW_SIZE, int CHUNK_SIZE, int BLOCK_SIZE>
+void ConcatVector<LCNT, WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE>::filter2(
+	input_window<float>* in0,
+	input_window<float>* in1,
+  output_window<float>* out
+) {
+  PROFILE_HEADER(printf(
+    "Running ConcatVector<%d, %d, %d>::filter2\n", WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE));
+
+  for (int i = 0; i < WINDOW_SIZE; i+=CHUNK_SIZE) {
+    int blockIdx = 0;
+    CAT_VEC(in0);
+    CAT_VEC(in1);
+  }
+
+  PROFILE_FOOTER;
+}
+
+template <int LCNT, int WINDOW_SIZE, int CHUNK_SIZE, int BLOCK_SIZE>
+void ConcatVector<LCNT, WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE>::filter1(
+	input_window<float>* in0,
+  output_window<float>* out
+) {
+  PROFILE_HEADER(printf(
+    "Running ConcatVector<%d, %d, %d>::filter7\n", WINDOW_SIZE, CHUNK_SIZE, BLOCK_SIZE));
+
+  for (int i = 0; i < WINDOW_SIZE; i+=CHUNK_SIZE) {
+    int blockIdx = 0;
+    CAT_VEC(in0);
   }
 
   PROFILE_FOOTER;
--- design/aie_src/concat.h
+++ design/aie_src/concat.h
@@ -4,6 +4,9 @@
 #include <adf.h>
 
 
+/*
+Function overhead is a very bad idea 163 -> 1047
+*/
 template <int LCNT, int WINDOW_SIZE, int CHUNK_SIZE, int BLOCK_SIZE>
 class ConcatScalar {
 	public:
@@ -84,6 +87,91 @@ class ConcatScalar {
 				REGISTER_FUNCTION(ConcatScalar::filter2);
 			} else if (LCNT == 1) {
 				REGISTER_FUNCTION(ConcatScalar::filter1);
+			}
+		}
+};
+
+
+template <int LCNT, int WINDOW_SIZE, int CHUNK_SIZE, int BLOCK_SIZE>
+class ConcatVector {
+	public:
+		void filter8(
+			input_window<float>* in0,
+			input_window<float>* in1,
+			input_window<float>* in2,
+			input_window<float>* in3,
+			input_window<float>* in4,
+			input_window<float>* in5,
+			input_window<float>* in6,
+			input_window<float>* in7,
+			output_window<float>* out
+		);
+		void filter7(
+			input_window<float>* in0,
+			input_window<float>* in1,
+			input_window<float>* in2,
+			input_window<float>* in3,
+			input_window<float>* in4,
+			input_window<float>* in5,
+			input_window<float>* in6,
+			output_window<float>* out
+		);
+		void filter6(
+			input_window<float>* in0,
+			input_window<float>* in1,
+			input_window<float>* in2,
+			input_window<float>* in3,
+			input_window<float>* in4,
+			input_window<float>* in5,
+			output_window<float>* out
+		);
+		void filter5(
+			input_window<float>* in0,
+			input_window<float>* in1,
+			input_window<float>* in2,
+			input_window<float>* in3,
+			input_window<float>* in4,
+			output_window<float>* out
+		);
+		void filter4(
+			input_window<float>* in0,
+			input_window<float>* in1,
+			input_window<float>* in2,
+			input_window<float>* in3,
+			output_window<float>* out
+		);
+		void filter3(
+			input_window<float>* in0,
+			input_window<float>* in1,
+			input_window<float>* in2,
+			output_window<float>* out
+		);
+		void filter2(
+			input_window<float>* in0,
+			input_window<float>* in1,
+			output_window<float>* out
+		);
+		void filter1(
+			input_window<float>* in0,
+			output_window<float>* out
+		);
+		static void registerKernelClass() {
+			if (LCNT == 8) {
+				REGISTER_FUNCTION(ConcatVector::filter8);
+			} else if (LCNT == 7) {
+				REGISTER_FUNCTION(ConcatVector::filter7);
+			} else if (LCNT == 6) {
+				REGISTER_FUNCTION(ConcatVector::filter6);
+			} else if (LCNT == 5) {
+				REGISTER_FUNCTION(ConcatVector::filter5);
+			} else if (LCNT == 4) {
+				REGISTER_FUNCTION(ConcatVector::filter4);
+			} else if (LCNT == 3) {
+				REGISTER_FUNCTION(ConcatVector::filter3);
+			} else if (LCNT == 2) {
+				REGISTER_FUNCTION(ConcatVector::filter2);
+			} else if (LCNT == 1) {
+				REGISTER_FUNCTION(ConcatVector::filter1);
 			}
 		}
 };
