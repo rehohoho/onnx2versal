34aca8606df092cc82e89a7456dd05cd053d7b4e
rehohoho <ernie.birdy.ho@gmail.com>
2023-05-15 01:05:22 -0700

    Python/gencpp: add Argmax and Conv

--- python/generate_cpp.py
+++ python/generate_cpp.py
@@ -29,3 +29,64 @@ class OpParser:
   
   def get_callarg_line(self) -> str:
     return f""
+
+
+class ArgmaxOp(OpParser):
+  include_file: str = "graph_argmax.h"
+
+
+class ConvOp(OpParser):
+  include_file: str = "graph_conv.h"
+
+  def register_params(self, 
+                      tinput: np.ndarray, 
+                      tweight: np.ndarray,
+                      tbias: np.ndarray,
+                      toutput: np.ndarray):
+    inB, inC, inH, inW = tinput.shape
+    wM, wC, wK1, wK2 = tweight.shape
+    bM, = tbias.shape
+    outB, outM, outH, outW = toutput.shape
+    
+    assert inH == inW and outH == outW and inC == wC and wM == bM and bM == outM \
+      and inB == outB and wK1 == wK2
+
+    self.INP_W = inH
+    self.OUT_W = outH
+    self.B = inB
+    self.C = inC
+    self.M = wM
+    self.K = wK1
+  
+  def register_weights(self, 
+                       weights: np.ndarray, 
+                       bias: np.ndarray):
+    """Expects MxCxKxK weights as per PyTorch
+    Returns MxCxKxK' weights, with K' padded so K'%8=0
+    """
+    M, C, K, K = weights.shape
+    k_pad = (8 - K%8) % 8
+    if k_pad != 0:
+      print(f"Padding Conv weights {M, C, K, K} to {M, C, K, K+k_pad}")
+      weights = np.pad(weights, ((0,0),(0,0),(0,0),(0,k_pad)), "constant", constant_values=0)
+    self.weights = weights
+    self.bias = bias
+  
+  def get_kernel_line(self) -> str:
+    return f"ConvReluGraph<Conv5x5on8ReluBCHW,{self.INP_W},{self.OUT_W},{self.B},{self.C},{self.M},{self.K}> {self.name};"
+
+  def get_arg_line(self) -> str:
+    return f"std::vector<float> {self.name}_w,\nstd::vector<float> {self.name}_b"
+  
+  def get_initlist_line(self) -> str:
+    return f"{self.name}({self.name}_w, {self.name}_b)"
+  
+  def get_weight_line(self) -> str:
+    wstring = str(self.weights.flatten().tolist())[1:-1]
+    bstring = str(self.bias.flatten().tolist())[1:-1]
+    return f"std::vector<float> {self.name}_w {{{wstring}}};\n" + \
+      f"std::vector<float> {self.name}_b {{{bstring}}};"
+  
+  def get_callarg_line(self) -> str:
+    return f"{self.name}_w, {self.name}_b"
+  
