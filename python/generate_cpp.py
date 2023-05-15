9270650399f168cccb561a01a1230f0d89966fe4
rehohoho <ernie.birdy.ho@gmail.com>
2023-05-15 01:05:45 -0700

    Python/gencpp: add Gemm

--- python/generate_cpp.py
+++ python/generate_cpp.py
@@ -90,3 +90,58 @@ class ConvOp(OpParser):
   def get_callarg_line(self) -> str:
     return f"{self.name}_w, {self.name}_b"
   
+
+class GemmOp(OpParser):
+  include_file: str = "graph_gemm.h"
+
+  def register_params(self, 
+                      tinput: np.ndarray, 
+                      tweight: np.ndarray,
+                      tbias: np.ndarray,
+                      toutput: np.ndarray):
+    inM, inK = tinput.shape
+    wN, wK = tweight.shape
+    bN, = tbias.shape
+    outM, outN = toutput.shape
+    
+    assert inM == outM and inK == wK and wN == bN and bN == outN
+
+    self.M = inM
+    self.K = inK
+    self.N = wN
+  
+  def register_weights(self, 
+                       weights: np.ndarray,
+                       bias: np.ndarray):
+    """Expects NxK weights as per PyTorch
+    Returns KxN weights, with N padded so N%4=0
+    """
+    weights = weights.transpose(1,0)
+    K, N = weights.shape
+    n_pad = (4 - N%4) % 4
+    if n_pad != 0:
+      print(f"Padding Gemm weights {K, N} to {K, N+n_pad}")
+      weights = np.pad(weights, ((0,0),(0,n_pad)), "constant", constant_values=0)
+    self.weights = weights
+    self.bias = bias
+  
+  def get_kernel_type(self) -> str:
+    return f"GemmReluMkknChunkGraph<GemmReluMKKN,ConcatVector,MAX_FLOAT_PARAMS/{self.K}/4*4,{self.M},{self.K},{self.N}>"
+  
+  def get_kernel_line(self) -> str:
+    return f"{self.get_kernel_type()} {self.name};"
+
+  def get_arg_line(self) -> str:
+    return f"std::vector<float> {self.name}_w,\nstd::vector<float> {self.name}_b"
+  
+  def get_initlist_line(self) -> str:
+    return f"{self.name}({self.name}_w, {self.name}_b)"
+  
+  def get_weight_line(self) -> str:
+    wstring = str(self.weights.flatten().tolist())[1:-1]
+    bstring = str(self.bias.flatten().tolist())[1:-1]
+    return f"std::vector<float> {self.name}_w {{{wstring}}};\n" + \
+      f"std::vector<float> {self.name}_b {{{bstring}}};"
+  
+  def get_callarg_line(self) -> str:
+    return f"{self.name}_w, {self.name}_b"
