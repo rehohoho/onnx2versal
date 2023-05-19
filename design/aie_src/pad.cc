6ce31244e8c9ba34cab05dfffec730ca23fdc7f1
rehohoho <ernie.birdy.ho@gmail.com>
2023-05-19 02:15:09 -0700

    Pad: add vector pad int16

--- design/aie_src/pad.cc
+++ design/aie_src/pad.cc
@@ -12,10 +12,39 @@ void PadScalar<TT, N, INP_W, OUT_W>::filter(
   
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < INP_W; j++) {
-      TT a = window_readincr(in);
-      window_writeincr(out, a);
+      window_writeincr(out, window_readincr(in));
     }
     window_incr(out, OUT_W - INP_W);
+  }
+
+  PROFILE_FOOTER;
+}
+
+
+template <typename TT, int N, int INP_W, int OUT_W>
+void PadVector<TT, N, INP_W, OUT_W>::filter(
+	input_window<TT>* in,  // NxINP_W
+  output_window<TT>* out // NxOUT_W
+) {
+  PROFILE_HEADER(printf(
+    "Running PadVector::filter<%d, %d>\n", INP_W, OUT_W));
+  
+  const int word_size = WORD_SIZE_BITS/sizeof(TT);
+
+  v8int16 v = null_v8int16();
+  
+  for (int i = 0; i < N; i++) {
+    int j = 0;
+    for (j; j < INP_W-7; j+=8) {
+      for (int k = 0; k < 8; k++) {
+        v = upd_elem(v, k, window_readincr(in));
+      }
+      window_writeincr(out, v);
+    }
+    for (j; j < INP_W; j++) {
+      window_writeincr(out, window_readincr(in));
+    }
+    window_incr(out, OUT_W-INP_W);
   }
 
   PROFILE_FOOTER;
--- design/aie_src/pad.h
+++ design/aie_src/pad.h
@@ -16,10 +16,13 @@
 
 /**
  * @brief Scalar implementation for Pad
- * PadScalar::filter<INP_W, OUT_W>, total = 373 takes 
+ * PadScalar::filter<int16_t, 28, 28, 32>, total = 5521
  */
 template <typename TT, int N, int INP_W, int OUT_W>
 class PadScalar {
+  private:
+    static const int WORD_SIZE_BYTES = 16;
+    
   public:
     void filter(
       input_window<TT>* in,  // NxINP_W
@@ -29,6 +32,28 @@ class PadScalar {
     static void registerKernelClass() {
       assert(INP_W<=OUT_W);
       REGISTER_FUNCTION(PadScalar::filter);
+    }
+};
+
+
+/**
+ * @brief Vector implementation for Pad
+ * PadVector::filter<int16_t, 28, 28, 32>, total = 5521
+ */
+template <typename TT, int N, int INP_W, int OUT_W>
+class PadVector {
+  private:
+    static const int WORD_SIZE_BITS = 16;
+
+  public:
+    void filter(
+      input_window<TT>* in,  // NxINP_W
+      output_window<TT>* out // NxOUT_W
+    );
+
+    static void registerKernelClass() {
+      assert(INP_W<=OUT_W);
+      REGISTER_FUNCTION(PadVector::filter);
     }
 };
 /** @}*/
