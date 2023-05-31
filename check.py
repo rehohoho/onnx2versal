5380906c70c6b1884de9a51a16eba3f50147e1bc
rehohoho <ernie.birdy.ho@gmail.com>
2023-05-30 23:51:28 -0700

    Python/check: add pass count when failed, update stdout format

--- check.py
+++ check.py
@@ -14,7 +14,7 @@ def load_txt(filepath: str):
   data = []
   for line in raw:
     if "TLAST" not in line and "T " not in line and line != "":
-      data.append([float(i) for i in line.split()])
+      data += [float(i) for i in line.split()]
   return np.array(data)
 
 
@@ -31,21 +31,17 @@ def is_file_match(res_path: str,
     data_arr = data_arr.reshape(*shape[:-1], -1)[..., :shape[-1]]
   
   if res_arr.shape == data_arr.shape:
-    if np.allclose(res_arr, data_arr, rtol=1e-03, atol=1e-05):
-      print(f"TEST (tolerance): OK! (rtol=1e-03, atol=1e-05)")
+    close_count = np.isclose(res_arr, data_arr, rtol=1e-03, atol=1e-05).sum()
+    if close_count == res_arr.size:
+      print(f"TEST: OK!")
       return True
     else:
-      print(f"TEST (tolerance): FAILED! (rtol=1e-03, atol=1e-05)")
+      print(f"TEST: FAILED! Only {close_count}/{res_arr.size} passed.")
       if is_throw_err == 0: import ipdb;ipdb.set_trace()
   
   else:
-    print(f"WARNING: res_arr shape {res_arr.shape}, data_arr shape {data_arr.shape}")
-    minSize = min(res_arr.size, data_arr.size)
-    if np.allclose(res_arr.flatten()[:minSize], data_arr.flatten()[:minSize], rtol=1e-03, atol=1e-05):
-      print(f"TEST (tolerance): first {minSize} OK! (rtol=1e-03, atol=1e-05)")
-    else:
-      print(f"TEST (tolerance): first {minSize} FAILED! (rtol=1e-03, atol=1e-05)")
-      if is_throw_err == 0: import ipdb;ipdb.set_trace()
+    print(f"TEST: FAILED! res_arr shape {res_arr.shape}, data_arr shape {data_arr.shape}")
+    if is_throw_err == 0: import ipdb;ipdb.set_trace()
 
   return False
 
@@ -68,23 +64,22 @@ if __name__ == "__main__":
       OUT_DIR = args.f1
       DATA_DIR = args.f2
     
-    print(f"Checking directories out: {OUT_DIR} and data: {DATA_DIR}")
+    print(f"Checking directories out: {OUT_DIR} and data: {DATA_DIR} at tolerance @ rtol=1e-03, atol=1e-05")
     res_filenames = os.listdir(OUT_DIR)
     
     filepairs = []
     for res_fn in res_filenames:
-      data_path = os.path.join(DATA_DIR, res_fn)
-      if os.path.exists(data_path):
-        filepairs.append((os.path.join(OUT_DIR, res_fn), data_path))
+      if os.path.exists(os.path.join(DATA_DIR, res_fn)):
+        filepairs.append((res_fn, res_fn))
         continue
-      
       data_basename = "_".join(res_fn.split("_")[:-1])+".txt"
-      parsed_data_path = os.path.join(DATA_DIR, data_basename)
-      if os.path.exists(parsed_data_path):
-        filepairs.append((os.path.join(OUT_DIR, res_fn), parsed_data_path))
-
+      if os.path.exists(os.path.join(DATA_DIR, data_basename)):
+        filepairs.append((res_fn, data_basename))
+      
     for i, (res_fn, data_fn) in enumerate(filepairs):
-      print(f"Checking {i+1}/{len(filepairs)}: {res_fn.replace(OUT_DIR, '')} against {data_fn.replace(DATA_DIR, '')}")
-      pass_count += is_file_match(res_fn, data_fn, args.err)
+      print(f"Checking {i+1}/{len(filepairs)}: {res_fn:<80} against {data_fn:<48}", end="\t")
+      pass_count += is_file_match(os.path.join(OUT_DIR, res_fn), 
+                                  os.path.join(DATA_DIR, data_fn), 
+                                  args.err)
   
   assert(pass_count == len(filepairs)), f"{pass_count} / {len(filepairs)} tests passed."
