1ae185b6e1ce6788ec22882e42fb07f940505d1d
rehohoho <ernie.birdy.ho@gmail.com>
2023-06-01 20:01:26 -0700

    softmax: add fastexp, double shift, and two approximations

--- design/aie_src/softmax.cc
+++ design/aie_src/softmax.cc
@@ -2,6 +2,37 @@
 #include "kernel_utils.h"
 
 
+// https://nic.schraudolph.org/pubs/Schraudolph99.pdf 
+double fastexp(float val) {
+  int a = float2fix(1512775 * val, 0);
+  long long b = (a + 1072632447);
+  b <<= 32;
+  double ans;
+  memcpy(&ans, &b, sizeof(ans));
+  return (float) ans;
+}
+
+// approximation with (1 + x/256)^256
+float fastexp2(float val, int precision) {
+  float ans = (1 + val*0.00390625);
+  for (int i = 0; i < precision; i++)
+    ans *= ans;
+  return ans;
+}
+
+// taylor series for e^x  
+template <int INP_H, int INP_W, int INP_W_PAD>
+float SoftmaxScalar<INP_H, INP_W, INP_W_PAD>::fastexp3(float val, int precision) {
+  float x = val;
+  float ans = 1;
+  for (int i = 0; i < precision; i++) {
+    ans += x * coef[i];
+    x *= x;
+  }
+  return ans;
+}
+
+
 template <int INP_H, int INP_W, int INP_W_PAD>
 void SoftmaxScalar<INP_H, INP_W, INP_W_PAD>::filter(
 	input_window<float>* in,
@@ -17,11 +48,9 @@ void SoftmaxScalar<INP_H, INP_W, INP_W_PAD>::filter(
     exp_scale = 0;
     for (int j = 0; j < INP_W; j++) {
       float a = window_readincr(in);
-      exp_v[j] = expf(a);
+      exp_v[j] = fastexp2(a, 8);
       exp_scale += exp_v[j];
-      printf("%f ", exp_v[j]);
     }
-    printf("\n");
     exp_scale = 1 / exp_scale;
     for (int j = 0; j < INP_W; j++) {
       window_writeincr(out, exp_v[j] * exp_scale);
--- design/aie_src/softmax.h
+++ design/aie_src/softmax.h
@@ -15,10 +15,18 @@
 
 /**
  * @brief Scalar implementation, 
- * SoftmaxScalar<10, 10> takes 250322 cycles
+ * SoftmaxScalar<10, 10> takes 
+ * - expf: 250322 cycles, within max abs diff 1e-05, max rel diff 1e-03
+ * - fastexp: 22200 cycles, max abs diff: 0.0046130717, max rel diff: 0.039221319418778974
+ * - fastexp2: 68282 cycles, max abs diff: 0.0002221614, max rel diff: 0.00129
+ * - fastexp3: 10484 cycles, max abs diff: 0.0010825096, max rel diff: 0.00956
  */
 template <int INP_H, int INP_W, int INP_W_PAD>
 class SoftmaxScalar {
+	private:
+	  float coef[10] = {1, 0.5, 0.16666666666666666, 0.041666666666666664, 0.008333333333333333, 0.001388888888888889, 0.0001984126984126984, 2.48015873015873e-05, 2.7557319223985893e-06, 2.755731922398589e-07};
+		float fastexp3(float val, int precision);
+
 	public:
 		void filter(
 			input_window<float>* in,
