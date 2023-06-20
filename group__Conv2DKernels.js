743a8fa44e2b39b6d6b68ef4f1f14afa72e27796
rehohoho <rehohoho@users.noreply.github.com>
2023-06-20 02:13:29 +0000

    deploy: f181a0df959ec2aef71532316a073314c6c7163f

--- group__Conv2DKernels.js
+++ group__Conv2DKernels.js
@@ -2,10 +2,11 @@ var group__Conv2DKernels =
 [
     [ "ConvReluScalarBHWC< INP_H, INP_W, OUT_W, STEP_H, STEP_W, B, C, M, K, IS_RELU >", "classConvReluScalarBHWC.html", null ],
     [ "ConvReluScalarBCHW< INP_H, INP_W, OUT_W, STEP_H, STEP_W, B, C, M, K, IS_RELU >", "classConvReluScalarBCHW.html", null ],
-    [ "Conv5x5ReluBCHW< INP_H, INP_W, OUT_W, STEP_H, STEP_W, B, C, M, _K_notused, IS_RELU >", "classConv5x5ReluBCHW.html", null ],
-    [ "Conv5x5on8ReluBCHW< INP_H, INP_W, OUT_W, STEP_H, STEP_W, B, C, M, _K_notused, IS_RELU >", "classConv5x5on8ReluBCHW.html", null ],
+    [ "Conv5x5ReluBCHW< INP_H, INP_W, OUT_W, STEP_H, STEP_W, B, C, M, K, IS_RELU >", "classConv5x5ReluBCHW.html", null ],
+    [ "Conv5x5on8ReluBCHW< INP_H, INP_W, OUT_W, STEP_H, STEP_W, B, C, M, K, IS_RELU >", "classConv5x5on8ReluBCHW.html", null ],
+    [ "Conv3x3on12ReluBCHW< INP_H, INP_W, OUT_W, STEP_H, STEP_W, B, C, M, K, IS_RELU >", "classConv3x3on12ReluBCHW.html", null ],
     [ "ConvReluScalarStreamCacheHW< INP_H, INP_W, OUT_W, STEP_H, STEP_W, B, C, M, K, IS_RELU >", "classConvReluScalarStreamCacheHW.html", null ],
     [ "ConvReluScalarStreamCacheCKK< INP_H, INP_W, OUT_W, STEP_H, STEP_W, B, C, M, K, IS_RELU >", "classConvReluScalarStreamCacheCKK.html", null ],
     [ "Conv3x3ReluStreamCacheCKK< INP_H, INP_W, OUT_W, STEP_H, STEP_W, B, C, M, K, IS_RELU >", "classConv3x3ReluStreamCacheCKK.html", null ],
-    [ "Conv3x3ReluStreamCacheCKK2Row< INP_H, INP_W, OUT_W, STEP_H, STEP_W, B, C, M, K, IS_RELU >", "classConv3x3ReluStreamCacheCKK2Row.html", null ]
+    [ "Conv3x3ReluStreamCacheCKKMultiRow< INP_H, INP_W, OUT_W, STEP_H, STEP_W, B, C, M, K, IS_RELU >", "classConv3x3ReluStreamCacheCKKMultiRow.html", null ]
 ];
