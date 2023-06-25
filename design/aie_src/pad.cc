24b96e407157aba318dfc2c9704e7bba535106eb
rehohoho <ernie.birdy.ho@gmail.com>
2023-06-25 12:23:06 +0800

    pad: fix case where W0+W1>16, or macro len=0

--- design/aie_src/pad.cc
+++ design/aie_src/pad.cc
@@ -82,22 +82,27 @@ void Pad2DStreamInt8<TT, B, INP_H, INP_W, H0, H1, W0, W1>::filter(
 	input_stream<TT>* restrict in,
   output_stream<TT>* restrict out
 ) {
+  printf("Pad2DStreamInt8<%s,%d,%d,%d,%d,%d,%d,%d>", typeid(TT).name(), B, INP_H, INP_W, H0, H1, W0, W1);
   PROFILE_HEADER2;
   
   v32int16 data = aie::broadcast<int16_t,32>(pad_value);
   v16int8 pad_value_v = aie::broadcast<int8_t,16>(pad_value);
   v16int16 pad_value_vint16 = aie::broadcast<int16_t,16>(pad_value);
-  unsigned data_offset = 0;
+  int data_offset = 0;
 
   // for top and bottom pads, assume OUT_W >= 16
 #define WRITE_PAD(out, len) \
   data = aie::shuffle_down((aie::vector<int16_t,32>) data, data_offset); \
   data = upd_w(data, 1, pad_value_vint16); \
-  data = aie::shuffle_down_replicate((aie::vector<int16_t,32>) data, 16-data_offset); \
-  put_wms(0, pack(ext_w(data, 0))); \
-  for (int i = 0; i <= (len - (16 - data_offset)) - 16; i+=16) \
-    put_wms(0, pad_value_v); \
-  data_offset = (len - (16 - data_offset)) & 0xf; \
+  if (data_offset + len >= 16) { \
+    data = aie::shuffle_down_replicate((aie::vector<int16_t,32>) data, 16-data_offset); \
+    put_wms(0, pack(ext_w(data, 0))); \
+    for (int i = 0; i <= (len - (16 - data_offset)) - 16; i+=16) \
+      put_wms(0, pad_value_v); \
+  } else { \
+    data = aie::shuffle_up((aie::vector<int16_t,32>) data, data_offset); \
+  } \
+  data_offset = (data_offset + len) & 0xf;
 
   for (int b = 0; b < B; b++) chess_prepare_for_pipelining chess_loop_range(B, B) {
     WRITE_PAD(out, H0*OUT_W+W0);
@@ -110,17 +115,7 @@ void Pad2DStreamInt8<TT, B, INP_H, INP_W, H0, H1, W0, W1>::filter(
         data = aie::shuffle_up((aie::vector<int16_t,32>) data, data_offset);
         put_wms(0, pack(ext_w(data, 0))); 
       }
-
-      data = aie::shuffle_down((aie::vector<int16_t,32>) data, data_offset);
-      data = upd_w(data, 1, pad_value_vint16);
-      if (data_offset + W0+W1 >= 16) {
-        data = aie::shuffle_down((aie::vector<int16_t,32>) data, 16-data_offset);
-        put_wms(0, pack(ext_w(data, 0)));
-        data_offset += -16+W0+W1;
-      } else {
-        data = aie::shuffle_up((aie::vector<int16_t,32>) data, data_offset);
-        data_offset += W0+W1;
-      }
+      WRITE_PAD(out, W0+W1);
     }
     WRITE_PAD(out, H1*OUT_W-W0);
   }
